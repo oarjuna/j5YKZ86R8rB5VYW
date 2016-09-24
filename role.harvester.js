97781a373780b26eaf7190a4cc6f739dcfc43dd2
Ryan Shannon <shannon@visionlink.org>
2016-09-23 22:57:13 -0600

    fixed

--- role.harvester.js
+++ role.harvester.js
@@ -1,17 +1,16 @@
 module.exports = {
     // a function to run the logic for this role
     run: function(creep) {
-        console.log("C: " + creep + " harvester");
         // if creep is bringing energy to a structure but has no energy left
         if (creep.memory.working == true && creep.carry.energy == 0) {
             // switch state
-            console.log("C: " + creep + " -- out of energy");
+            console.log(creep + " -- harvester -- out of energy");
             creep.memory.working = false;
         }
         // if creep is harvesting energy but is full
         else if (creep.memory.working == false && creep.carry.energy == creep.carryCapacity) {
             // switch state
-            console.log("C: " + creep + " -- energy capacity full");
+            console.log("C: " + creep + " -- harvester -- energy capacity full");
             creep.memory.working = true;
         }
 
@@ -30,27 +29,27 @@ module.exports = {
 
             // if we found one
             if (structure != undefined) {
-                console.log("C: " + creep + " -- transfering.");
+                console.log("C: " + creep + " -- harvester -- transfering.");
                 // try to transfer energy, if it is not in range
                 if (creep.transfer(structure, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
                     // move towards it
-                    console.log("C: " + creep + " -- moving to drop off");
+                    console.log("C: " + creep + " -- harvester -- moving to drop off");
                     creep.moveTo(structure);
                 }
             }
 	    else {
-		console.log(creep + " -- did not find a structure to xfer to!");
+		console.log(creep + " -- harvester -- did not find a structure to xfer to!");
 	    }
         }
         // if creep is supposed to harvest energy from source
         else {
             // find closest source
-            console.log("C: " + creep + " -- looking for energy source");
+            console.log("C: " + creep + " -- harvester -- looking for energy source");
             var source = creep.pos.findClosestByPath(FIND_SOURCES);
             // try to harvest energy, if the source is not in range
             if (creep.harvest(source) == ERR_NOT_IN_RANGE) {
                 // move towards the source
-                console.log("C: " + creep + " -- moving to harvest");
+                console.log("C: " + creep + " -- harvester -- moving to harvest");
                 creep.moveTo(source);
             }
         }
