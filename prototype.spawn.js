9a328edc3353b69032958aba8de7f44bcdc78dfe
Ryan Shannon <shannon@visionlink.org>
2016-09-24 11:26:55 -0600

    yay

--- prototype.spawn.js
+++ prototype.spawn.js
@@ -3,7 +3,7 @@ module.exports = function() {
     StructureSpawn.prototype.createCustomCreep =
         function(energy, roleName) {
             // create a balanced body as big as possible with the given energy
-	   /*
+	   
             var numberOfParts = Math.floor(energy / 200);
             var body = [];
             for (let i = 0; i < numberOfParts; i++) {
@@ -15,9 +15,9 @@ module.exports = function() {
             for (let i = 0; i < numberOfParts; i++) {
                 body.push(MOVE);
             }
-	    */
+	    
 
-	    var body = ["WORK","WORK","MOVE","MOVE","CARRY","CARRY"];
+	    //var body = ["WORK","WORK","MOVE","MOVE","CARRY","CARRY"];
 
             // create creep with the created body and the given role
             return this.createCreep(body, undefined, { role: roleName, working: false, destid: null });
