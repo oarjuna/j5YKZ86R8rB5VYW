c93bc9a499a196c4e64badc61894f448537ee906
Ryan Shannon <shannon@visionlink.org>
2016-09-24 14:16:20 -0600

    yay

--- role.remoteharvester.js
+++ role.remoteharvester.js
@@ -23,8 +23,8 @@ module.exports = {
 
 	    var room = Game.spawns.Spawn1.room;
 	    var containersWithEnergy = room.find(FIND_STRUCTURES, {
-		        filter: (i) => i.structureType == STRUCTURE_CONTAINER && 
-		    			i.structureType == STRUCTURE_TOWER// &&
+		        filter: (i) => i.structureType == STRUCTURE_CONTAINER// && 
+		    			//i.structureType == STRUCTURE_TOWER// &&
 		                       //i.store[RESOURCE_ENERGY] < 2000
 	    });
 	    console.log("CT: " + containersWithEnergy);
