f1d4d4832e0a998b875256e794f9941a7af1c204
Ryan Shannon <shannon@visionlink.org>
2016-09-24 07:34:38 -0600

    yay

--- main.js
+++ main.js
@@ -81,6 +81,7 @@ module.exports.loop = function () {
 
     var status1 = "H: " + numberOfHarvesters + "/" + minimumNumberOfHarvesters + " U: " + numberOfUpgraders + "/" + minimumNumberOfUpgraders;
     var status2 = "R: " + numberOfRepairers + "/" + minimumNumberOfRepairers + " B: " + numberOfBuilders + "/" + minimumNumberOfBuilders;
+    var status3 = "RH: " + numberOfRemoteHarvesters ;
     console.log(status1 + " " + status2);
 
 
