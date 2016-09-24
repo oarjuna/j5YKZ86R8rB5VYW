8310096ea108f138a36fd61f8c1143524588e6fb
Ryan Shannon <shannon@visionlink.org>
2016-09-24 08:53:33 -0600

    yay

--- role.remoteharvester.js
+++ role.remoteharvester.js
@@ -30,7 +30,7 @@ module.exports = {
         // if creep is supposed to harvest energy from source
         else {
 	    if (creep.memory._move == undefined ) {
-		    console.log("undef");
+		creep.moveTo(Game.flags.Flag1);
 	    }
 	    //console.log(creep + " -- remoteharv -- in " + creep.memory._move.room);
 	    if (creep.memory._move.room == 'E58N3' ) {
