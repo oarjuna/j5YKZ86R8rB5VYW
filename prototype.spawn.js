8a7a8cd3c346e947102f8b38b549c16640b3104a
Ryan Shannon <shannon@visionlink.org>
2016-09-25 07:16:25 -0600

    yay

--- prototype.spawn.js
+++ prototype.spawn.js
@@ -2,25 +2,27 @@ module.exports = function() {
     // create a new function for StructureSpawn
     StructureSpawn.prototype.createCustomCreep =
         function(energy, roleName) {
-            // create a balanced body as big as possible with the given energy
-	   
-            var numberOfParts = Math.floor(energy / 200);
-            var body = [];
-            for (let i = 0; i < numberOfParts; i++) {
-                body.push(WORK);
-            }
-            for (let i = 0; i < numberOfParts; i++) {
-                body.push(CARRY);
-            }
-            for (let i = 0; i < numberOfParts; i++) {
-                body.push(MOVE);
-            }
-	    
 
-	    //var body = ["WORK","WORK","MOVE","MOVE","CARRY","CARRY"];
-	    console.log = ("spawn: " + body);
-
-            // create creep with the created body and the given role
-            return this.createCreep(body, undefined, { role: roleName, working: false, destid: null });
-        };
+        if (roleName == 'harvester') {
+	    var body = ["WORK","WORK","MOVE","MOVE","CARRY","CARRY"];
+        }
+        else if (roleName == 'upgrader') {
+	    var body = ["WORK","WORK","MOVE","MOVE","CARRY","CARRY"];
+        }
+        else if (roleName == 'builder') {
+	    var body = ["WORK","WORK","MOVE","MOVE","CARRY","CARRY"];
+        }
+        else if (roleName == 'repairer') {
+	    var body = ["WORK","WORK","MOVE","MOVE","CARRY","CARRY"];
+        }
+        else if (roleName == 'wallRepairer') {
+	    var body = ["WORK","WORK","MOVE","MOVE","CARRY","CARRY"];
+        }
+        else if (roleName == 'remote_harvester') {
+	    var body = ["WORK","WORK","MOVE","MOVE","CARRY","CARRY"];
+        }
+	console.log = ("spawn: " + body);
+        // create creep with the created body and the given role
+        return this.createCreep(body, undefined, { role: roleName, working: false, destid: null });
+     };
 };
