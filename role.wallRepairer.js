7268418a835e615d295d2833ae5ec75dab71705a
Ryan Shannon <shannon@visionlink.org>
2016-09-24 21:17:24 -0600

    yay

--- role.wallRepairer.js
+++ role.wallRepairer.js
@@ -3,7 +3,6 @@ var roleBuilder = require('role.builder');
 module.exports = {
     // a function to run the logic for this role
     run: function(creep) {
-        console.log("C: " + creep + " wallRepairer");
         // if creep is trying to repair something but has no energy left
         if (creep.memory.working == true && creep.carry.energy == 0) {
             // switch state
