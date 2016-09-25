775aaefbe2b9b0bf35772f519d9b335d5b5da791
Ryan Shannon <shannon@visionlink.org>
2016-09-25 09:48:21 -0600

    yay

--- prototype.spawn.js
+++ prototype.spawn.js
@@ -42,11 +42,11 @@ module.exports = function() {
         else if (roleName == "remote_harvester") {
 	    var body = [
 			WORK, WORK, // 200
-		    	MOVE, MOVE, MOVE, MOVE, // 200
+		    	MOVE, MOVE, MOVE, MOVE, MOVE, MOVE,// 200
 			CARRY, CARRY, // 100
 			ATTACK, // 80
                       	TOUGH, TOUGH // 20
-		] // 700
+		] // 600
         }
 	/* ACTION - COST - DESC 
 	 * MOVE - 50 - Decreases fatigue by 2 points per tick.
