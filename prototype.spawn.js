ecb4c17dd3c4abb2dd663aa1196a61c179986967
Ryan Shannon <shannon@visionlink.org>
2016-09-25 09:11:17 -0600

    yay

--- prototype.spawn.js
+++ prototype.spawn.js
@@ -41,7 +41,7 @@ module.exports = function() {
         }
         else if (roleName == "remote_harvester") {
 	    var body = [WORK, WORK, // 200
-		    	MOVE, MOVE, MOVE, MOVE // 200
+		    	MOVE, MOVE, MOVE, MOVE, // 200
 			CARRY, CARRY // 100
 			] // 500
 	    //var body = [WORK, WORK,  // 200
