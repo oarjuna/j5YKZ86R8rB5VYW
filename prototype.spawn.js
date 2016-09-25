6ba9f764b4262bec515f923d9d8a3beb1c31b850
Ryan Shannon <shannon@visionlink.org>
2016-09-25 07:30:29 -0600

    yay

--- prototype.spawn.js
+++ prototype.spawn.js
@@ -12,24 +12,18 @@ module.exports = function() {
 	    var body = ["WORK","WORK","MOVE","MOVE","CARRY","CARRY"];
         }
         else if (roleName == 'builder') {
-	    console.log = ("spawn: " + body);
-	    var body = ["WORK","WORK","MOVE","MOVE","CARRY","CARRY"];
-        }
-        else if (roleName == 'repairer') {
-	    console.log = ("spawn: " + body);
-	    var body = ["WORK","WORK","MOVE","MOVE","CARRY","CARRY"];
-        }
-        else if (roleName == 'wallRepairer') {
-	    console.log = ("spawn: " + body);
-	    var body = ["WORK","WORK","MOVE","MOVE","CARRY","CARRY"];
-        }
-        else if (roleName == "remote_harvester") {
 	    var body = [WORK, WORK, MOVE, MOVE, CARRY, CARRY];
 	    console.log = ("spawn: " + body);
         }
-	else { 
-		console.log = ("spawn: " + body);
-	}
+        else if (roleName == 'repairer') {
+	    var body = ["WORK","WORK","MOVE","MOVE","CARRY","CARRY"];
+        }
+        else if (roleName == 'wallRepairer') {
+	    var body = ["WORK","WORK","MOVE","MOVE","CARRY","CARRY"];
+        }
+        else if (roleName == "remote_harvester") {
+        }
+	console.log = ("spawn: " + body);
         // create creep with the created body and the given role
         return this.createCreep(body, undefined, { role: roleName, working: false, destid: null });
      };
