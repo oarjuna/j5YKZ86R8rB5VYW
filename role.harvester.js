cc6ac88ca9ce0316f9bb62b4acb11cdca8f20445
Ryan Shannon <shannon@visionlink.org>
2016-09-24 19:50:11 -0600

    yay

--- role.harvester.js
+++ role.harvester.js
@@ -23,7 +23,6 @@ module.exports = {
                 // we use the arrow operator to define it
                filter: (s) => (s.structureType == STRUCTURE_SPAWN
                          || s.structureType == STRUCTURE_EXTENSION
-                         || s.structureType == STRUCTURE_CONTROLLER
                          || s.structureType == STRUCTURE_TOWER)
                              && s.energy < s.energyCapacity
             });
