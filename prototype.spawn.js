ffc4af8356b9a54859731f25819c5b159b1dd9d6
Ryan Shannon <shannon@visionlink.org>
2016-09-25 07:32:31 -0600

    yay

--- prototype.spawn.js
+++ prototype.spawn.js
@@ -9,21 +9,21 @@ module.exports = function() {
         }
         else if (roleName == 'upgrader') {
 	    console.log = ("spawn: " + body);
-	    var body = ["WORK","WORK","MOVE","MOVE","CARRY","CARRY"];
+	    var body = ["WORK","WORK","MOVE","MOVE","CARRY","CARRY"]
         }
         else if (roleName == 'builder') {
-	    var body = [WORK, WORK, MOVE, MOVE, CARRY, CARRY];
-	    console.log = ("spawn: " + body);
+	    var body = [WORK, WORK, MOVE, MOVE, CARRY, CARRY]
+	    console.log = ("spawn: " + body)
         }
         else if (roleName == 'repairer') {
-	    var body = ["WORK","WORK","MOVE","MOVE","CARRY","CARRY"];
+	    var body = ["WORK","WORK","MOVE","MOVE","CARRY","CARRY"]
         }
         else if (roleName == 'wallRepairer') {
-	    var body = ["WORK","WORK","MOVE","MOVE","CARRY","CARRY"];
+	    var body = ["WORK","WORK","MOVE","MOVE","CARRY","CARRY"]
         }
         else if (roleName == "remote_harvester") {
         }
-	console.log = ("spawn: " + body);
+	console.log = ("spawn: " + body)
         // create creep with the created body and the given role
         return this.createCreep(body, undefined, { role: roleName, working: false, destid: null });
      };
