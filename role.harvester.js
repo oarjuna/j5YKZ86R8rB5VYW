10ad51d14a7a538358d5cc0edc7af5fd1b8d3474
Ryan Shannon <shannon@visionlink.org>
2016-09-24 18:46:50 -0600

    yay

--- role.harvester.js
+++ role.harvester.js
@@ -61,7 +61,7 @@ module.exports = {
 		if ( c_source != undefined ) { var source = c_source; }
 		else { var source = creep.pos.findClosestByPath(FIND_SOURCES); }
 
-                	console.log(creep + " -- harvester -- " +  source");
+               	console.log(creep + " -- harvester -- " +  source");
        	    	creep.say("harv");
             	// try to harvest energy, if the source is not in range
             	if (creep.harvest(source) == ERR_NOT_IN_RANGE) {
