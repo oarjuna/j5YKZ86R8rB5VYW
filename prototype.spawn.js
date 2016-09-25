0feb0dc4466077ad2e864c60cd57d640c4723318
Ryan Shannon <shannon@visionlink.org>
2016-09-25 09:32:32 -0600

    yay

--- prototype.spawn.js
+++ prototype.spawn.js
@@ -43,7 +43,7 @@ module.exports = function() {
 	    var body = [
 			WORK, WORK, // 200
 		    	MOVE, MOVE, MOVE, MOVE, // 200
-			CARRY, CARRY // 100
+			CARRY, CARRY, // 100
 			ATTACK, // 80
                       	TOUGH, TOUGH // 20
 		] // 700
