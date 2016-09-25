8b332811d75993a83eceec58dc54355e17f9e14f
Ryan Shannon <shannon@visionlink.org>
2016-09-25 07:34:49 -0600

    yay

--- prototype.spawn.js
+++ prototype.spawn.js
@@ -22,7 +22,8 @@ module.exports = function() {
         else if (roleName == "remote_harvester") {
 	    var body = [WORK, WORK, MOVE, MOVE, CARRY, CARRY]
         }
-	console.log = ("spawn: " + body)
+
+	console.log = ("spawn: " + body);
         // create creep with the created body and the given role
         return this.createCreep(body, undefined, { role: roleName, working: false, destid: null });
      };
