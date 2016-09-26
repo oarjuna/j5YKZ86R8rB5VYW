8ccd3eaf047cdee64b2dac35c8ca944a2323513c
Ryan Shannon <shannon@visionlink.org>
2016-09-25 18:07:02 -0600

    yay

--- role.claimer.js
+++ role.claimer.js
@@ -19,20 +19,14 @@ module.exports = {
 	//creep.memory.working = true;
         // if creep is supposed to transfer energy to the controller
         if (creep.memory.working == true) {
-            // instead of claimerController we could also use:
-            // if (creep.transfer(creep.room.controller, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-
-            if (creep.memory.destid == null ) {
 		var claim_room = ["579faa610700be0674d30e47","E58N3","E58N4","E59N3","E58N2"];
 		// 579faa610700be0674d30e47 -- E57N3
 		var claimers_tmp = _.sum(Game.creeps, (c) => c.memory.role == 'claimer');
 		claimers_tmp = claimers_tmp - 1;
-		//console.log(creep + " XXX  -- " + claimers_tmp);
-            	var target_controller = Game.getObjectById(claim_room[claimers_tmp]);
+          	var target_controller = Game.getObjectById(claim_room[claimers_tmp]);
 		console.log(creep + " XXX  -- " + target_controller);
-            }
 
-            // try to upgrade the controller
+            	// try to upgrade the controller
             if (creep.reserveController(target_controller) == ERR_NOT_IN_RANGE) {
                 // if not in range, move towards the controller
 		console.log(creep + " -- claimer -- moving to  " + target_controller);
