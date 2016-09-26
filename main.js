c1a680e2189dc7a51912a6f18e90bd76b81a2d21
Ryan Shannon <shannon@visionlink.org>
2016-09-26 17:57:00 -0600

    yay

--- main.js
+++ main.js
@@ -73,7 +73,7 @@ module.exports.loop = function () {
 
     // setup some minimum numbers for different roles
     var minimumNumberOfHarvesters = 3;
-    var minimumNumberOfRemoteHarvesters = 25;
+    var minimumNumberOfRemoteHarvesters = 10;
     var minimumNumberOfLocalHarvesters = 1;
     var minimumNumberOfUpgraders = 1;
     var minimumNumberOfBuilders = 3;
