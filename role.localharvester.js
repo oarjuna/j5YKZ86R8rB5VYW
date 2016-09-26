39a6d29f64b0e0fa39763290568990239a782c22
Ryan Shannon <shannon@visionlink.org>
2016-09-26 17:46:04 -0600

    yay

--- role.localharvester.js
+++ role.localharvester.js
@@ -6,39 +6,35 @@ module.exports = {
         // if creep is trying to complete a constructionSite but has no energy left
         if (creep.memory.working == true && creep.carry.energy == 0) {
             // switch state
-            console.log(creep + " -- builder -- out of energy.");
+            console.log(creep + " -- localharv -- energy store empty.");
             creep.memory.working = false;
         }
         // if creep is harvesting energy but is full
         else if (creep.memory.working == false && creep.carry.energy == creep.carryCapacity) {
             // switch state
-            console.log(creep + " -- builder -- energy storage full.");
+            console.log(creep + " -- localharv -- energy storage full.");
             creep.memory.working = true;
         }
 
         // if creep is supposed to complete a constructionSite
         if (creep.memory.working == true) {
             // find closest constructionSite
-            var constructionSite = creep.pos.findClosestByPath(FIND_CONSTRUCTION_SITES);
+	    var container_id = '57e9afb83cd48b64657b2501'; // north container
+            var container = Game.getObjectById(container_id);
             // if one is found
-            if (constructionSite != undefined) {
-                // try to build, if the constructionSite is not in range
-                if (creep.build(constructionSite) == ERR_NOT_IN_RANGE) {
-                    // move towards the constructionSite
-		    creep.say("build");
-                    creep.moveTo(constructionSite);
+            if (container != undefined) {
+                // try to build, if the container is not in range
+                if (creep.transfer(container) == ERR_NOT_IN_RANGE) {
+                    // move towards the container
+                    creep.moveTo(container);
                 }
-            }
-            // if no constructionSite is found
-            else {
-                // go upgrading the controller
-                roleUpgrader.run(creep);
             }
         }
         // if creep is supposed to harvest energy from source
         else {
             // find closest source
-            var source = creep.pos.findClosestByPath(FIND_SOURCES);
+	    var source_id = '579faa710700be0674d30fd7'; // north source
+            var source = Game.getObjectById(source_id);
             // try to harvest energy, if the source is not in range
             if (creep.harvest(source) == ERR_NOT_IN_RANGE) {
                 // move towards the source
