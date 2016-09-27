953d68bb00b9d441028be92bd8dcff39d4459303
Ryan Shannon <shannon@visionlink.org>
2016-09-27 10:13:58 -0600

    yay

--- role.builder.js
+++ role.builder.js
@@ -50,7 +50,7 @@ module.exports = {
     		});
 
 
-		console.log(creep + " -- TAR: " + target2);
+		console.log(creep + " -- TAR: " + source);
             // try to transfer energy, if the source is not in range
             if (creep.transfer(source, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
                 // move towards the source
