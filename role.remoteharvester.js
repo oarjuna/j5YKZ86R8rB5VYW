4d026aaa8cc441da0dd691a074bac9b64dda1043
Ryan Shannon <shannon@visionlink.org>
2016-09-27 11:24:10 -0600

    yay

--- role.remoteharvester.js
+++ role.remoteharvester.js
@@ -3,7 +3,7 @@ module.exports = {
     run: function(creep) {
 	
 	// attack hostile creeps
-	var target = creep.pos.findClosestByRange(FIND_CONSTRUCTION_SITES);
+	var target = creep.pos.findClosestByRange(FIND_HOSTILE_SPAWNS);
 	//var target = creep.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
 console.log(creep + " ALERT -- found hostile creep!" + creep.attack(target));
 	if(target) {
