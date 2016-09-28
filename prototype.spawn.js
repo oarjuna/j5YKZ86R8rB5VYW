2d373df04fc97e7a0f5f8dfbdc97a0bfb0b64620
Ryan Shannon <shannon@visionlink.org>
2016-09-28 11:52:17 -0600

    yay

--- prototype.spawn.js
+++ prototype.spawn.js
@@ -88,6 +88,6 @@ module.exports = function() {
 	 */
 
         // create creep with the created body and the given role
-        return this.createCreep(body, undefined, { role: roleName, working: false, destid: null });
+        return this.createCreep(body, undefined, { role: roleName, working: false, destid: destid });
      };
 };
