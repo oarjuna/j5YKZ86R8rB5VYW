91d5a31a0160c6485573b5d1eb54730c7253c430
Ryan Shannon <shannon@visionlink.org>
2016-09-28 13:33:11 -0600

    yay

--- main.js
+++ main.js
@@ -163,7 +163,7 @@ module.exports.loop = function () {
 	else { var dest = '579faa710700be0674d30fd8'; } // south
         console.log("main -- spawning local_harvester -- " + dest);
 
-        name = Game.spawns.Spawn1.createCustomCreep(energy, 'local_harvester',dest);
+        //name = Game.spawns.Spawn1.createCustomCreep(energy, 'local_harvester',dest);
     }
     // if not enough remote harvesters
     else if (numberOfRemoteHarvesters < minimumNumberOfRemoteHarvesters) {
