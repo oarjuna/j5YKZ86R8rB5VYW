308d3f449f390200db8e22d8c6939bf20a554fee
Ryan Shannon <shannon@visionlink.org>
2016-09-28 22:08:52 -0600

    yay

--- role.harvester.js
+++ role.harvester.js
@@ -46,8 +46,8 @@ module.exports = {
         else {
                // find closest container with energy
                 var container = creep.pos.findClosestByRange(FIND_STRUCTURES, {
-                //filter: (s) => ( s.structureType==STRUCTURE_CONTAINER &&
-                filter: (s) => ( s.structureType==STRUCTURE_STORAGE &&
+                filter: (s) => ( s.structureType==STRUCTURE_CONTAINER &&
+                //filter: (s) => ( s.structureType==STRUCTURE_STORAGE &&
                                s.store[RESOURCE_ENERGY] > 500
 			       });
 		console.log(creep + " -- harv --pickup -- " + structure );
