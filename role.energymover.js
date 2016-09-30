d193b192362eed0dac7204034aa7f9be9ed71ee3
Ryan Shannon <shannon@visionlink.org>
2016-09-30 14:51:46 -0600

    yay

---
+++ role.energymover.js
@@ -0,0 +1,48 @@
+module.exports = {
+    // a function to run the logic for this role
+    run: function(creep) {
+		// find nearby link
+		var closelink = Game.spawns.Spawn1.pos.findInRange(FIND_MY_STRUCTURES, 2, 
+				{filter: {
+					structureType: STRUCTURE_LINK
+				}})[0];
+
+		// find the far away link
+		var farlink = Game.spawns.Spawn1.pos.findInRange(FIND_MY_STRUCTURES,
+                                {filter: {
+                                        structureType: STRUCTURE_LINK
+                                }})[1];
+
+
+		// find nearby xfer destinations ( controller. storage, other?)
+                var storage = Game.spawns.Spawn1.pos.findInRange(FIND_MY_STRUCTURES, 5,
+                                {filter: {
+                                        structureType: STRUCTURE_STORAGE 
+                                }})[0];
+
+               var controller = Game.spawns.Spawn1.pos.findInRange(FIND_MY_STRUCTURES, 2,
+                                {filter: {
+                                        structureType: STRUCTURE_CONTROLLER
+                                }})[0];
+
+		console.log(creep " CL: " + closelink + " FL: " + farlink +
+				" ST: " + storage + " CT: " + controller);
+
+		if ( controller != undefined ) {
+			// load up on energy
+			status = creep.withdraw(closelink, RESOURCE_ENERGY);
+                	console.log(creep + " mover load: " + closelink + " status: " + status);
+			// upgrade the controller
+			status = creep.upgradeController(controller)
+                	console.log(creep + " mover xfer: " + closelink + " ct: "
+				 + controller + " status: " + status);
+		}		
+		elsif ( storage != undefined ) { 
+			// load up on energy
+			status = creep.withdraw(storage, RESOURCE_ENERGY);
+			// xfer to the far link
+			status = closelink.transferEnergy(farlink);
+		}
+
+    }
+};
