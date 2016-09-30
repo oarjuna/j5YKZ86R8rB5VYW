8903ddd67b98a074c3d7e0b61ab32e3a996f524f
Ryan Shannon <shannon@visionlink.org>
2016-09-30 16:30:34 -0600

    yay

--- role.energymover.js
+++ role.energymover.js
@@ -49,7 +49,7 @@ module.exports = {
 				// load up on energy
 				status = creep.withdraw(storage, RESOURCE_ENERGY);
 				// xfer energy to the link
-				status2 = creep.transfer(nearlink, RESOURCE_ENERGY);
+				status2 = creep.transfer(closelink, RESOURCE_ENERGY);
 				// xfer to the far link
 				status = closelink.transferEnergy(farlink);
 				console.log(creep + " link xferstatus: " + status );
