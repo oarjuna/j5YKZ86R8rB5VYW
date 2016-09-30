6287b91783e2e4bd4e24ab5597703c8fc74b9228
Ryan Shannon <shannon@visionlink.org>
2016-09-29 19:47:09 -0600

    yay

--- func.shared.js
+++ func.shared.js
@@ -5,6 +5,8 @@ module.exports = {
 	{
 		//console.log(creep + "TESTING SHARED FUNC " + foo);
                // find closest container with energy and fill up
+  	       // shared.pickupEnergy(creep);
+
                 var container = creep.pos.findClosestByRange(FIND_STRUCTURES, {
                 filter: (s) => ( s.structureType==STRUCTURE_CONTAINER ||
                                 s.structureType==STRUCTURE_STORAGE ) &&
