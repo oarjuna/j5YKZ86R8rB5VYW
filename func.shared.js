c855bf1d5dfb55bee32028a67d6f64249b9b7924
Ryan Shannon <shannon@visionlink.org>
2016-09-29 19:02:18 -0600

    yay

--- func.shared.js
+++ func.shared.js
@@ -1,7 +1,7 @@
 module.exports = {
 	// create a new function for pickup energy 
 	// pickup energy from the closest container
-	Creep.prototype.pickupEnergy : function(foo)
+	Creep.prototype.pickupEnergy: function(foo)
 		{
 			console.log(creep + "TESTING SHARED FUNC" + foo);
 		};
