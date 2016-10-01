5b8f920a47e48a0f0917a14155262fc997c72dfc
Ryan Shannon <shannon@visionlink.org>
2016-10-01 16:19:36 -0600

    yay

--- role.soldier.js
+++ role.soldier.js
@@ -19,19 +19,24 @@ module.exports = {
 			filter: (s) => s.structureType==STRUCTURE_EXTENSION
 	});
 
-        if(hostile_tower != undefined ) {
-                console.log(creep + "ALERT -- attacking hostile tower!" + hostile_tower);
+        if( hostile_tower != undefined ) {
+                console.log(creep + "SOLDIER -- attacking hostile tower!" + hostile_tower);
                 if(creep.attack(hostile_tower) == ERR_NOT_IN_RANGE) {
                         creep.moveTo(hostile_tower);
                 }
         }
 	else if ( creep_target != undefined ) {
-		console.log(creep + "ALERT -- attacking hostile creep!" + creep_target);
+		console.log(creep + "SOLDIER -- attacking hostile creep!" + creep_target);
                 if(creep.attack(creep_target) == ERR_NOT_IN_RANGE) {
                         creep.moveTo(creep_target);
                 }
-
 	}
+        else if ( other_target != undefined ) {
+                console.log(creep + "SOLDIER -- attacking hostile target!" + other_target);
+                if(creep.attack(other_target) == ERR_NOT_IN_RANGE) {
+                        creep.moveTo(other_target);
+                }
+        }
 
         // are we in the room with the flag?
         if ( creep.pos.roomName != gotoFlag.pos.roomName ) {
@@ -40,7 +45,9 @@ module.exports = {
                 console.log(creep + " soldier -movestatus " + moveStatus + " GF " + gotoFlag);
                 console.log(creep + " soldier -CR " + creep.pos.roomName + " FR: " + gotoFlag.pos.roomName);
         }
-
+	else {
+		creep.moveTo(gotoFlag);
+	}
 
 
 // end
