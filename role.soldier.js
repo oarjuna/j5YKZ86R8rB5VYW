31bdc2d9f791476b63c2b5a7d443f0e1cd49696f
Ryan Shannon <shannon@visionlink.org>
2016-10-01 15:57:41 -0600

    yay

--- role.soldier.js
+++ role.soldier.js
@@ -1,7 +1,30 @@
 module.exports = {
     // a function to run the logic for this role
     run: function(creep) {
-	// stuff goes here
+        creep.say("^-!");
+
+        // get your assignment
+        var dest_key = creep.memory.destid;
+        if ( dest_key == 'Attack') {  var gotoFlag = Game.flags.Attack; }
+	else { var gotoFlag = Game.spawns.Spawn1; }
+
+        // attack hostile creeps
+        var target = creep.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
+        if(target) {
+                console.log(creep + "ALERT -- found hostile creep!" + target);
+                if(creep.attack(target) == ERR_NOT_IN_RANGE) {
+                        creep.moveTo(target);
+                }
+        }
+
+        // are we in the room with the flag?
+        if ( creep.pos.roomName != gotoFlag.pos.roomName && creep.memory.working != true ) {
+                // not in the room with the flag, move towards the flag
+                moveStatus = creep.moveTo(gotoFlag);
+                console.log(creep + " remoteharv -movestatus " + moveStatus + " GF " + gotoFlag);
+                console.log(creep + " removeharv -CR " + creep.pos.roomName + " FR: " + gotoFlag.pos.roomName);
+        }
+// end
     }
 };
 
