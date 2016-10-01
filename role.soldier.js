3bfba9f51349f61b07ee8d3db4b18ac1cc331650
Ryan Shannon <shannon@visionlink.org>
2016-10-01 16:16:27 -0600

    yay

--- role.soldier.js
+++ role.soldier.js
@@ -6,16 +6,32 @@ module.exports = {
         // get your assignment
         var dest_key = creep.memory.destid;
         if ( dest_key == 'Attack') {  var gotoFlag = Game.flags.Attack; }
-	else { var gotoFlag = creep.room.controller; }
+	else { var gotoFlag = creep.room.spawn; }
 
-        // attack hostile creeps
-        var target = creep.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
-        if(target) {
-                console.log(creep + "ALERT -- found hostile creep!" + target);
-                if(creep.attack(target) == ERR_NOT_IN_RANGE) {
-                        creep.moveTo(target);
+        // collect targets
+        var creep_target = creep.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
+
+	var hostile_tower = creep.pos.findClosestByRange(FIND_HOSTILE_STRUCTURES, {
+			filter: (s) => s.structureType==STRUCTURE_TOWER 
+	});
+
+	var other_target = creep.pos.findClosestByRange(FIND_HOSTILE_STRUCTURES, {
+			filter: (s) => s.structureType==STRUCTURE_EXTENSION
+	});
+
+        if(hostile_tower != undefined ) {
+                console.log(creep + "ALERT -- attacking hostile tower!" + hostile_tower);
+                if(creep.attack(hostile_tower) == ERR_NOT_IN_RANGE) {
+                        creep.moveTo(hostile_tower);
                 }
         }
+	else if ( creep_target != undefined ) {
+		console.log(creep + "ALERT -- attacking hostile creep!" + creep_target);
+                if(creep.attack(creep_target) == ERR_NOT_IN_RANGE) {
+                        creep.moveTo(creep_target);
+                }
+
+	}
 
         // are we in the room with the flag?
         if ( creep.pos.roomName != gotoFlag.pos.roomName ) {
