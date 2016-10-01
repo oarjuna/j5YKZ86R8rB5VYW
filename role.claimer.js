fe7ce615b9b48b6ca0bece2617f24523c46d43d8
Ryan Shannon <shannon@visionlink.org>
2016-10-01 08:08:17 -0600

    yay

--- role.claimer.js
+++ role.claimer.js
@@ -35,7 +35,7 @@ module.exports = {
                                 }
                         }
                         // construction jobs
-                        else if ( constructionSite != undefined ) {
+                        else if ( constructionSite != undefined  && creep.carry.energy != 0) {
                                 if ( creep.build(constructionSite) == ERR_NOT_IN_RANGE ) {
                                         console.log(creep + " claimer -- CS " + constructionSite);
                                         creep.moveTo(constructionSite);
@@ -48,7 +48,7 @@ module.exports = {
 
 
 			// repair jobs
-			else if ( repairStructure != undefined ) {
+			else if ( repairStructure != undefined  && creep.carry.energy != 0) {
                                 if ( creep.repair(repairStructure) == ERR_NOT_IN_RANGE ) {
                                         console.log(creep + " claimer -- RS " + repairStructure);
                                         creep.moveTo(repairStructure);
