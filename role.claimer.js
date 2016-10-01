36946e211375258a86423a55d07d3d2968ec152a
Ryan Shannon <shannon@visionlink.org>
2016-10-01 07:48:17 -0600

    yay

--- role.claimer.js
+++ role.claimer.js
@@ -9,11 +9,11 @@ module.exports = {
                 else if ( dest_key == 'Flag3') {  var gotoFlag = Game.flags.Flag4; }
 		
 		// are we in the room with the flag?
-		if ( creep != gotoFlag ) {
+		if ( creep.pos.roomname != gotoFlag ) {
 			// not in the room with the flag, move towards the flag
 			moveStatus = creep.moveTo(gotoFlag);
 			console.log(creep + " claimer -movestatus " + moveStatus + " GF " + gotoFlag);	
-			console.log(creep + " claimer -creeproom " + creep.room + " FR " + creep.memory.destid.room.name);	
+			console.log(creep + " claimer -creeproom " + creep.pos.roomname + " FR " + creep.memory.destid.room.name);	
 		}
 		else {
 			// in the room with the flag, look for stuff to do (likely very CPU heavy)
