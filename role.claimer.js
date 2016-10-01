bd82543ad878934a60d1cf21a3255f69667d0a12
Ryan Shannon <shannon@visionlink.org>
2016-09-30 18:50:39 -0600

    yay

--- role.claimer.js
+++ role.claimer.js
@@ -1,26 +1,17 @@
 module.exports = {
-	// a function to run the logic for this role
 	run: function(creep) {
 		creep.say("!!");
-        	// if creep is bringing energy to the controller but has no energy left
-        	if (creep.memory.working == true && creep.carry.energy == 0) {
-            	// switch state
-	    	console.log(creep + " -- claimer -- out of energy");
-            	creep.memory.working = false;
-        	}
-        	// if creep is harvesting energy but is full
-        	else if (creep.memory.working == false && creep.carry.energy == creep.carryCapacity) {
-            	// switch state
-	    	console.log(creep + " -- claimer -- energy capacity full");
-            	creep.memory.working = true;
-            	creep.memory.destid = null;
-        	}
+		// get your assignment
+		var gotoFlag = Game.getObjectById(creep.memory.destid);	
 
-        	// if creep is supposed to transfer energy to the controller
-        	if (creep.memory.working == true) {
-			//
+		// move towards it
+		moveStatus = creep.moveTo(gotoFlag);
+		if ( moveStatus != 0 ) {
+			
 		}
-        	else {
+		else {
+
 		}
+
 	}
 };
