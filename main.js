7ddebf0d1557c778b0cbf37636e1dfcc88553ed1
Ryan Shannon <shannon@visionlink.org>
2016-10-01 15:38:52 -0600

    yay

--- main.js
+++ main.js
@@ -21,9 +21,7 @@ module.exports.loop = function () {
         }
     }
 
-    // for every creep name in Game.creeps
     for (let name in Game.creeps) {
-        // get the creep object
         var creep = Game.creeps[name];
         if (creep.memory.role == 'harvester') {  roleHarvester.run(creep); }
         else if (creep.memory.role == 'local_harvester') {  roleLocalHarvester.run(creep); }
