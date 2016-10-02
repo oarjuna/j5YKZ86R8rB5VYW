c442b05200cd3d03708698bce4396a2be34847c0
Ryan Shannon <shannon@visionlink.org>
2016-10-02 17:55:31 -0600

    yay

--- prototype.spawn.js
+++ prototype.spawn.js
@@ -33,7 +33,6 @@ module.exports = function() {
 			CARRY, CARRY, CARRY, CARRY // 200
 		]; // 650
         }
-	// m - 50 / w - 100 / c - 50 / a - 80 / t - 10
         else if (roleName == 'repairer') {
 		var body = [
                         WORK, WORK,
@@ -51,7 +50,8 @@ module.exports = function() {
         else if (roleName == "remote_harvester") {
 	    var body = [
 			WORK, WORK, // 200
-		    	MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, // 550
+		    	MOVE, MOVE, MOVE, MOVE, MOVE, MOVE,
+			 MOVE, MOVE, MOVE, MOVE, MOVE, // 550
 			CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, // 300
 			ATTACK, // 80
                       	TOUGH, TOUGH // 20
@@ -85,7 +85,8 @@ module.exports = function() {
         }
         else if (roleName == "energy_mover") {
                 var body = [
-                        WORK, WORK, WORK, WORK, WORK, WORK, WORK, WORK, WORK, WORK,// 1000
+                        WORK, WORK, WORK, WORK, WORK, 
+			WORK, WORK, WORK, WORK, WORK,// 1000
                         MOVE,   // 50
                         CARRY  // 50
                 ]; // 1100
@@ -104,22 +105,6 @@ module.exports = function() {
                         MOVE,MOVE,MOVE,MOVE,MOVE
                         ];
         }
-
-	/* ACTION - COST - DESC 
-	 * MOVE - 50 - Decreases fatigue by 2 points per tick.
-	 * WORK - 100 - faster work based on task
-	 * CARRY - 50 - Can contain up to 50 resource units.
-	 * ATTACK - 80 - Attacks another creep/structure with 30 hits per tick in a short-ranged attack.
-	 * RANGED_ATTACK - 150 - Attacks another single creep/structure with 10 hits 
-	 * 			per tick in a long-range attack up to 3 squares long.
-	 * 			Attacks all hostile creeps/structures within 3 squares 
-	 * 			range with 1-4-10 hits (depending on the range).
-	 * HEAL - 250 - Heals self or another creep restoring 12 hits per tick 
-	 * 			in short range or 4 hits per tick at a distance.
-	 * CLAIM - 600 - Claims a neutral room controller. ( see doc )
-	 * TOUGH - 10 - 	No effect, just additional hit points to the creep's body. 
-	 * 			Can be boosted to resist damage.
-	 */
 
         // create creep with the created body and the given role
         return this.createCreep(body, undefined, { role: roleName, working: false, destid: dest, birthplace: birthplace});
