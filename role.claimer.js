f112feb4d3c9bef572f4f3c7ff5a90c26778e042
Ryan Shannon <shannon@visionlink.org>
2016-10-02 12:29:29 -0600

    yay

--- role.claimer.js
+++ role.claimer.js
@@ -10,7 +10,7 @@ module.exports = {
         else if ( dest_key == 'Flag4') {  var gotoFlag = Game.flags.Flag4; }
         else if ( dest_key == 'Flag5') {  var gotoFlag = Game.flags.Flag5; }
 
-	console.log(creep + " body " creep.body);
+	console.log(creep + " body " + creep.body);
 
         // attack hostile creeps
         var target = creep.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
