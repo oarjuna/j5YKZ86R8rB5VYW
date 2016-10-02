a37800ff88e5e5d2ff39129b9cfe50e770b8bce3
Ryan Shannon <shannon@visionlink.org>
2016-10-02 11:28:58 -0600

    yay

--- role.claimer.js
+++ role.claimer.js
@@ -59,7 +59,8 @@ module.exports = {
                                 }
                         }
 			// upgrade the controller if necessary
-			else if ( upgradeControl != undefined && 
+			else if ( false  &&
+				upgradeControl != undefined && 
 				creep.memory.working == true &&
 				creep.pos.roomName == gotoFlag.pos.roomName
 				) {
