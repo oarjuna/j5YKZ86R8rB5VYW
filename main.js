0d5885a69b50088a6e8ef0f96e013f1a13aeefe7
Ryan Shannon <shannon@visionlink.org>
2016-10-02 16:26:55 -0600

    yay

--- main.js
+++ main.js
@@ -29,7 +29,6 @@ module.exports.loop = function () {
         	else if (creep.memory.role == 'repairer') { roleRepairer.run(creep); }
         	else if (creep.memory.role == 'wallRepairer') { roleWallRepairer.run(creep); }
         	else if (creep.memory.role == 'remote_harvester') { roleRemoteHarvester.run(creep); }
-	creep.memory.birthplace = 'Spawn1';
     	}
 
     	// Tower control
