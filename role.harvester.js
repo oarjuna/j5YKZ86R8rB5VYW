55f10ba07e59be16777bf615b9f2cf68f641abb1
Ryan Shannon <shannon@visionlink.org>
2016-10-02 17:29:08 -0600

    yay

--- role.harvester.js
+++ role.harvester.js
@@ -54,6 +54,7 @@ module.exports = {
                                s.store[RESOURCE_ENERGY] > 199
                 });
 
+		if ( container == undefined ) { var container = creep.creep.pos.findClosestByRange(FIND_SOURCE); }
                 //console.log(creep + " harv --pickup -- " + container );
                 creep.say("pickup");
 
