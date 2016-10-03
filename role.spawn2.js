56ffffb3d1f35dfca6814bd557dd12921257bab6
Ryan Shannon <shannon@visionlink.org>
2016-10-03 09:37:08 -0600

    yay

--- role.spawn2.js
+++ role.spawn2.js
@@ -5,8 +5,8 @@ module.exports = {
         // spawn levels
         var spawn2MinHarv = 1;
         var spawn2MinReHa = 0;
-        var spawn2MinLoHa = 1;
-        var spawn2MinUgra = 4;
+        var spawn2MinLoHa = 2;
+        var spawn2MinUgra = 3;
         var spawn2MinBuil = 0;
         var spawn2MinClai = 0;
         var spawn2MinSold = 0;
