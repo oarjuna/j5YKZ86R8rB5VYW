1ebfbb7f82dd8d256a666e7ca620835b8fc2bf54
Ryan Shannon <shannon@visionlink.org>
2016-10-05 20:08:27 -0600

    yay

--- role.spawn1.js
+++ role.spawn1.js
@@ -14,7 +14,7 @@ module.exports = {
         var spawn1MinSold = 0;
         var spawn1MinRepa = 0;
         var spawn1MinWall = 0; //1
-        var spawn1MinEner = 3; //3
+        var spawn1MinEner = 2; //3
 
         // count the number of creeps alive for each role born at Spawn1
         var numSpawn1Harv = _.sum(Game.creeps, (c) => c.memory.role == 'harvester' && c.memory.birthplace == 'Spawn1');
