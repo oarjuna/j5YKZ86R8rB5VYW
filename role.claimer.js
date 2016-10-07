d54b78f158d9e31096f3370c0c2c656d82ee4f6d
Ryan Shannon <shannon@visionlink.org>
2016-10-07 07:52:00 -0600

    yay

--- role.claimer.js
+++ role.claimer.js
@@ -1,6 +1,6 @@
 module.exports = {
 	run: function(creep) {
-	creep.say("!!");
+	creep.say("[-]");
 
 	// array search function
 	function contains(a, obj) {
@@ -33,10 +33,7 @@ module.exports = {
 		
 		// are we in the room with the flag?
 		if ( creep.pos.roomName != gotoFlag.pos.roomName ) {
-			// not in the room with the flag, move towards the flag
 			moveStatus = creep.moveTo(gotoFlag);
-			////console.log(creep + " claimer -movestatus " + moveStatus + " GF " + gotoFlag);	
-			//console.log(creep + " claimer -CR " + creep.pos.roomName + " FR: " + gotoFlag.pos.roomName);
 		}
 		else {
 			// in the room with the flag, look for stuff to do (likely very CPU heavy)
