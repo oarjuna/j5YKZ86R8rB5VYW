aa0f024d1f906ab8df1b53aa41c56d247e8dc162
Ryan Shannon <shannon@visionlink.org>
2017-08-23 11:29:18 -0600

    yep

--- role.new-harvester.js
+++ role.new-harvester.js
@@ -37,6 +37,7 @@ module.exports = {
 					console.log(creep + " " + creep.room.controller.ticksToDowngrade);
 				}
 				else {
+          creep.say("struc -- " + structure);
 					roleBuilder.run(creep);
 				}
 			}
