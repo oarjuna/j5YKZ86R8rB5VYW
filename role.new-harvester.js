b25642cb02b34ac2fac5eb56b18a75cb0d6d4e78
unknown <ryan.shannon@gmail.com>
2017-08-23 19:47:38 -0600

    yay

--- role.new-harvester.js
+++ role.new-harvester.js
@@ -26,7 +26,7 @@ module.exports = {
       // CONTAINERS
       var structure_container = creep.pos.findClosestByRange(FIND_STRUCTURES, {
           filter: (s) => (
-            ( s.structureType == STRUCTURE_CONTAINER)
+            ( s.structureType == STRUCTURE_CONTAINER )
           )});
 
       // SPAWN
