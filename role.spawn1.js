98eeb5bd1f67827e58afeb3670a5aff9640c1fcf
unknown <ryan.shannon@gmail.com>
2017-08-24 10:12:30 -0600

    fasdf

--- role.spawn1.js
+++ role.spawn1.js
@@ -170,8 +170,8 @@ module.exports = {
                          "/" + roomSp1.controller.progressTotal +
                         " lvl: " + roomSp1.controller.level;
 
-	var status13Sp1 = " Stored -- " + Game.spawns.Spawn1.room.storage.store[RESOURCE_ENERGY];
-	//var status13Sp1 = " Stored -- R ";
+	//var status13Sp1 = " Stored -- " + Game.spawns.Spawn1.room.storage.store[RESOURCE_ENERGY];
+	var status13Sp1 = " Stored -- R ";
 
         // print the thing
         console.log("Sp1: " + status11Sp1 + status12Sp1 + status13Sp1);
