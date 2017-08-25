a3ff12a6813e609cfed2447917afbb478437bc9d
unknown <ryan.shannon@gmail.com>
2017-08-25 10:32:26 -0600

    dfd

--- role.spawn1.js
+++ role.spawn1.js
@@ -10,7 +10,7 @@ module.exports = {
         var spawn1MinWall = 0; // 1
         var spawn1MinEner = 0; // 1
 
-        var spawn1MinReHa = 0;
+        var spawn1MinReHa = 1;
         var spawn1MinClai = 0;
         var spawn1MinSold = 0;
         var spawn1MinRepa = 0;
