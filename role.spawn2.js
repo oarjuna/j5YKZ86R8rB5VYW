f443fb3794065af03bbf887868cc4d18dde6a929
unknown <ryan.shannon@gmail.com>
2017-08-25 12:40:54 -0600

    sdfgd

--- role.spawn2.js
+++ role.spawn2.js
@@ -4,7 +4,7 @@ module.exports = {
     run: function(foo) {
         // spawn levels
         var spawn2MinHarv = 3; // 3
-        var spawn2MinDeli = 4; // 3
+        var spawn2MinDeli = 6; // 3
         var spawn2MinUgra = 0; // 3
         var spawn2MinBuil = 1; // 1
         var spawn2MinWall = 0; // 1
