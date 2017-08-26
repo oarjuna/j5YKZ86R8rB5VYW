eb1e6acd4cd2d2af7b306a3d646ba7da22cc52a2
unknown <ryan.shannon@gmail.com>
2017-08-26 16:46:07 -0600

    asdfsd

--- main.js
+++ main.js
@@ -24,17 +24,20 @@ var roleSpawn3 = require('role.spawn3');
 */
   // spawn levels
 
-  function Empire(spawn_name) {
-    this.spawn = spawn_name;
+  var Empire = new Object();
+
+  function Spawn(spawn_name,creep_levels) {
+    this.name   = spawn_name;
+    this.levels  = creep_levels;
   }
 
-  var scl1 = [ 4, 4, 5, 1, 0, 0, 1, 0, 0];
-  var scl2 = [ 3, 7, 1, 1, 0, 0, 1, 0, 0];
-  var scl3 = [ 2, 1, 0, 0, 0, 0, 0, 0, 0];
+  var scl[0] = [ 4, 4, 5, 1, 0, 0, 1, 0, 0];
+  var scl[1] = [ 3, 7, 1, 1, 0, 0, 1, 0, 0];
+  var scl[2] = [ 2, 1, 0, 0, 0, 0, 0, 0, 0];
 
   for (var cur_spawn_tmp in Game.spawns){
      spawn_name = Game.spawns[cur_spawn_tmp].name;
-     var tmp_foo = new Empire(spawn_name)
+     var tmp_foo = new Spaw(spawn_name)
    }
 
    console.log("KPP: -- " + Empire.spawn);
@@ -64,9 +67,9 @@ module.exports.loop = function () {
   Harvest, Deliv, Upgrade, Buid, Energy, RemoHarv, Claim, Soldier, Repair
 */
   // Run the spawn logics
-  roleSpawn1.run(scl1[0],scl1[1],scl1[2],scl1[3],scl1[4],scl1[5],scl1[6],scl1[7],scl1[8]);
-  roleSpawn2.run(scl2[0],scl2[1],scl2[2],scl2[3],scl2[4],scl2[5],scl2[6],scl2[7],scl2[8]);
-  //roleSpawn3.run(scl3[0],scl3[1],scl3[2],scl3[3],scl3[4],scl3[5],scl3[6],scl3[7],scl3[8]);
+  roleSpawn1.run(scl[0][0],scl[0][1],scl[0][2],scl[0][3],scl[0][4],scl[0][5],scl[0][6],scl[0][7],scl[0][8]);
+  roleSpawn2.run(scl[1][0],scl[1][1],scl[1][2],scl[1][3],scl[1][4],scl[1][5],scl[1][6],scl[1][7],scl[1][8]);
+  //roleSpawn3.run(scl[2][0],scl[2][1],scl[2][2],scl[2][3],scl[2][4],scl[2][5],scl[2][6],scl[2][7],scl[2][8]);
 
 	gameStatus.display_status(1);
 
