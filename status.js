e4c99a24fb443d587aa6b714121b3a420a41971d
unknown <ryan.shannon@gmail.com>
2017-08-26 14:14:33 -0600

    adsfsd

--- status.js
+++ status.js
@@ -62,7 +62,7 @@ module.exports = {
                         " lvl: " + cur_room.controller.level;
 
     	//var status13 = " Stored -- " +  _.sum(cur_room.storage.store[RESOURCE_ENERGY]);
-    	var status13 = " energy_avail -- " + energy_avail;
+    	//var status13 = " energy_avail -- " + energy_avail;
 
             // print the thing
             console.log(": " + status11 + status12 + status13);
