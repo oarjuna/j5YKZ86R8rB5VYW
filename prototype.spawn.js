e844afc6cd8e6547610c758ad0f17f17bf889ee7
unknown <ryan.shannon@gmail.com>
2017-08-26 11:50:41 -0600

    dsfsd

--- prototype.spawn.js
+++ prototype.spawn.js
@@ -24,50 +24,50 @@ module.exports = function() {
 
     // 300 - 549 energy --- initial spawn
     if ( energy < 550 ) {
-      if (roleName == 'harvester')      { var body = [ WORK, MOVE, MOVE, WORK, CARRY ]; // 300 }
-      else if (roleName == 'builder')   { var body = [ WORK, MOVE, MOVE, WORK, CARRY ]; // 300 }
-      else if (roleName == 'repairer')  { var body = [ WORK, MOVE, MOVE, WORK, CARRY ]; // 300 }
-      else if (roleName == 'upgrader')  { var body = [ WORK, MOVE, MOVE, WORK, CARRY ]; // 300 }
-      else if (roleName == 'deliverer') { var body = [ WORK, MOVE, MOVE, WORK, CARRY ]; // 300 }
+      if (roleName == 'harvester')      { var body = [ WORK, MOVE, MOVE, WORK, CARRY ]; }// 300 }
+      else if (roleName == 'builder')   { var body = [ WORK, MOVE, MOVE, WORK, CARRY ]; }// 300 }
+      else if (roleName == 'repairer')  { var body = [ WORK, MOVE, MOVE, WORK, CARRY ]; }// 300 }
+      else if (roleName == 'upgrader')  { var body = [ WORK, MOVE, MOVE, WORK, CARRY ]; }// 300 }
+      else if (roleName == 'deliverer') { var body = [ WORK, MOVE, MOVE, WORK, CARRY ]; }// 300 }
     }
 
     // 550 - 799 energy
     else if ( energy >= 550 && energy < 800 ) {
-      if (roleName == 'harvester')              { var body = [ WORK, WORK, WORK, MOVE, MOVE, MOVE, CARRY, CARRY ]; //550 }
-      else if (roleName == 'deliverer')         { var body = [ WORK, WORK, MOVE, MOVE, MOVE, CARRY, CARRY, CARRY, CARRY ]; // 550 }
-      else if (roleName == 'upgrader')          { var body = [WORK, WORK, MOVE, MOVE, MOVE, CARRY, CARRY, CARRY, CARRY ]; // 550 }
-      else if (roleName == 'builder')           { var body = [ WORK, WORK, MOVE, MOVE, MOVE, MOVE, CARRY, CARRY, CARRY ]; // 550 }
-      else if (roleName == 'repairer')          { var body = [ WORK, WORK, WORK, MOVE, MOVE, MOVE, CARRY, CARRY ]; //  550  }
-      else if (roleName == "remote_harvester")  { var body = [ WORK, WORK, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, CARRY, CARRY, CARRY, CARRY ]; // 700 }
-      else if (roleName == "claimer")           { var body = [ WORK, WORK, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, CARRY, CARRY, CARRY, CARRY ]; //  700 }
-      else if (roleName == "claimer_lg")        { var body = [ CLAIM, MOVE, MOVE]; roleName = "claimer"; // 700 }
+      if (roleName == 'harvester')              { var body = [ WORK, WORK, WORK, MOVE, MOVE, MOVE, CARRY, CARRY ]; } //550 }
+      else if (roleName == 'deliverer')         { var body = [ WORK, WORK, MOVE, MOVE, MOVE, CARRY, CARRY, CARRY, CARRY ]; } // 550 }
+      else if (roleName == 'upgrader')          { var body = [WORK, WORK, MOVE, MOVE, MOVE, CARRY, CARRY, CARRY, CARRY ]; } // 550 }
+      else if (roleName == 'builder')           { var body = [ WORK, WORK, MOVE, MOVE, MOVE, MOVE, CARRY, CARRY, CARRY ]; } // 550 }
+      else if (roleName == 'repairer')          { var body = [ WORK, WORK, WORK, MOVE, MOVE, MOVE, CARRY, CARRY ]; } //  550  }
+      else if (roleName == "remote_harvester")  { var body = [ WORK, WORK, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, CARRY, CARRY, CARRY, CARRY ]; }
+      else if (roleName == "claimer")           { var body = [ WORK, WORK, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, CARRY, CARRY, CARRY, CARRY ]; } //  700 }
+      else if (roleName == "claimer_lg")        { var body = [ CLAIM, MOVE, MOVE]; roleName = "claimer"; } // 700 }
     }
 
     // 800 energy
     else if ( energy >= 800 && energy < 1100 ) {
-      if (roleName == 'harvester')              { var body = [ WORK, WORK, WORK, MOVE, MOVE, MOVE, CARRY, CARRY ]; //550 }
-      else if (roleName == 'deliverer')         { var body = [ WORK, WORK, MOVE, MOVE, MOVE, CARRY, CARRY, CARRY, CARRY ]; // 550 }
-      else if (roleName == 'upgrader')          { var body = [WORK, WORK, MOVE, MOVE, MOVE, CARRY, CARRY, CARRY, CARRY ]; // 550 }
-      else if (roleName == 'builder')           { var body = [ WORK, WORK, MOVE, MOVE, MOVE, MOVE, CARRY, CARRY, CARRY ]; // 550 }
-      else if (roleName == 'repairer')          { var body = [ WORK, WORK, WORK, MOVE, MOVE, MOVE, CARRY, CARRY ]; //  550  }
-      else if (roleName == "remote_harvester")  { var body = [ WORK, WORK, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, CARRY, CARRY, CARRY, CARRY ]; // 700 }
-      else if (roleName == "claimer")           { var body = [ WORK, WORK, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, CARRY, CARRY, CARRY, CARRY ]; //  700 }
-      else if (roleName == "claimer_lg")        { var body = [ CLAIM, MOVE, MOVE]; roleName = "claimer"; // 700 }
-      else if (roleName == "soldier")           { var body = [ TOUGH,TOUGH,TOUGH,TOUGH,TOUGH,MOVE,MOVE,MOVE,MOVE,MOVE,MOVE,MOVE,MOVE,MOVE,ATTACK,ATTACK,ATTACK,ATTACK]; // 900 }
+      if (roleName == 'harvester')              { var body = [ WORK, WORK, WORK, MOVE, MOVE, MOVE, CARRY, CARRY ];  }//550 }
+      else if (roleName == 'deliverer')         { var body = [ WORK, WORK, MOVE, MOVE, MOVE, CARRY, CARRY, CARRY, CARRY ]; } // 550 }
+      else if (roleName == 'upgrader')          { var body = [WORK, WORK, MOVE, MOVE, MOVE, CARRY, CARRY, CARRY, CARRY ];  }// 550 }
+      else if (roleName == 'builder')           { var body = [ WORK, WORK, MOVE, MOVE, MOVE, MOVE, CARRY, CARRY, CARRY ];  }// 550 }
+      else if (roleName == 'repairer')          { var body = [ WORK, WORK, WORK, MOVE, MOVE, MOVE, CARRY, CARRY ];  }//  550  }
+      else if (roleName == "remote_harvester")  { var body = [ WORK, WORK, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, CARRY, CARRY, CARRY, CARRY ]; } // 700 }
+      else if (roleName == "claimer")           { var body = [ WORK, WORK, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, CARRY, CARRY, CARRY, CARRY ]; } //  700 }
+      else if (roleName == "claimer_lg")        { var body = [ CLAIM, MOVE, MOVE]; roleName = "claimer";  }// 700 }
+      else if (roleName == "soldier")           { var body = [ TOUGH,TOUGH,TOUGH,TOUGH,TOUGH,MOVE,MOVE,MOVE,MOVE,MOVE,MOVE,MOVE,MOVE,MOVE,ATTACK,ATTACK,ATTACK,ATTACK];  }// 900 }
     }
 
     // >= 1100 energy
     else if ( energy >= 1100 ){
-      if (roleName == 'harvester')              { var body = [ WORK, WORK, WORK, MOVE, MOVE, MOVE, CARRY, CARRY ]; //550 }
-      else if (roleName == 'deliverer')         { var body = [ WORK, WORK, MOVE, MOVE, MOVE, CARRY, CARRY, CARRY, CARRY ]; // 550 }
-      else if (roleName == 'upgrader')          { var body = [WORK, WORK, MOVE, MOVE, MOVE, CARRY, CARRY, CARRY, CARRY ]; // 550 }
-      else if (roleName == 'builder')           { var body = [ WORK, WORK, MOVE, MOVE, MOVE, MOVE, CARRY, CARRY, CARRY ]; // 550 }
-      else if (roleName == 'repairer')          { var body = [ WORK, WORK, WORK, MOVE, MOVE, MOVE, CARRY, CARRY ]; //  550  }
-      else if (roleName == "remote_harvester")  { var body = [ WORK, WORK, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, CARRY, CARRY, CARRY, CARRY ]; // 700 }
-      else if (roleName == "claimer")           { var body = [ WORK, WORK, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, CARRY, CARRY, CARRY, CARRY ]; //  700 }
-      else if (roleName == "claimer_lg")        { var body = [ CLAIM, MOVE, MOVE]; roleName = "claimer"; // 700 }
-      else if (roleName == "soldier")           { var body = [ TOUGH,TOUGH,TOUGH,TOUGH,TOUGH,MOVE,MOVE,MOVE,MOVE,MOVE,MOVE,MOVE,MOVE,MOVE,ATTACK,ATTACK,ATTACK,ATTACK]; // 900 }
-      else if (roleName == "energy_mover")      { var body = [ WORK, WORK, WORK, WORK, WORK, MOVE, CARRY, CARRY ]; // 1100}
+      if (roleName == 'harvester')              { var body = [ WORK, WORK, WORK, MOVE, MOVE, MOVE, CARRY, CARRY ]; } //550 }
+      else if (roleName == 'deliverer')         { var body = [ WORK, WORK, MOVE, MOVE, MOVE, CARRY, CARRY, CARRY, CARRY ]; } // 550 }
+      else if (roleName == 'upgrader')          { var body = [WORK, WORK, MOVE, MOVE, MOVE, CARRY, CARRY, CARRY, CARRY ]; } // 550 }
+      else if (roleName == 'builder')           { var body = [ WORK, WORK, MOVE, MOVE, MOVE, MOVE, CARRY, CARRY, CARRY ]; } // 550 }
+      else if (roleName == 'repairer')          { var body = [ WORK, WORK, WORK, MOVE, MOVE, MOVE, CARRY, CARRY ]; } //  550  }
+      else if (roleName == "remote_harvester")  { var body = [ WORK, WORK, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, CARRY, CARRY, CARRY, CARRY ]; } // 700 }
+      else if (roleName == "claimer")           { var body = [ WORK, WORK, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, CARRY, CARRY, CARRY, CARRY ];  }//  700 }
+      else if (roleName == "claimer_lg")        { var body = [ CLAIM, MOVE, MOVE]; roleName = "claimer";  }// 700 }
+      else if (roleName == "soldier")           { var body = [ TOUGH,TOUGH,TOUGH,TOUGH,TOUGH,MOVE,MOVE,MOVE,MOVE,MOVE,MOVE,MOVE,MOVE,MOVE,ATTACK,ATTACK,ATTACK,ATTACK]; } // 900 }
+      else if (roleName == "energy_mover")      { var body = [ WORK, WORK, WORK, WORK, WORK, MOVE, CARRY, CARRY ]; } // 1100}
     }
 
     // create creep with the created body and the given role
