33baf6f0e6f9552c700536d052838d621d05719c
unknown <ryan.shannon@gmail.com>
2017-08-27 16:22:35 -0600

    ssss

--- prototype.spawn.js
+++ prototype.spawn.js
@@ -34,7 +34,7 @@ module.exports = function() {
     }
 
     // 800 energy_avail
-    else if ( energy_avail >= 800 && energy_avail < 1100 ) {
+    else if ( energy_avail >= 800 && energy_avail < 1000 ) {
       if      (roleName == 'harvester')   { var body = [ WORK,WORK,WORK, WORK, WORK, WORK, MOVE, MOVE, CARRY, CARRY ];  }         // 800
       else if (roleName == 'deliverer')   { var body = [ WORK,WORK,WORK, WORK, MOVE, MOVE, MOVE, MOVE, CARRY, CARRY, CARRY, CARRY ]; }  // 800
       else if (roleName == 'upgrader')    { var body = [ WORK,WORK,WORK, WORK, MOVE, MOVE, MOVE, CARRY, CARRY, CARRY, CARRY ];  } // 750
@@ -48,7 +48,7 @@ module.exports = function() {
     }
 
     // >= 1100 energy_avail
-    else if ( energy_avail >= 1100 ){
+    else if ( energy_avail >= 1000 ){
       if      (roleName == 'harvester')   { var body = [ WORK,WORK,WORK, WORK, WORK, WORK, MOVE, MOVE, CARRY, CARRY ];  }         // 800
       else if (roleName == 'deliverer')   { var body = [ WORK,WORK,WORK, WORK, MOVE, MOVE, MOVE, CARRY, CARRY, CARRY, CARRY ]; }  // 750
       else if (roleName == 'upgrader')    { var body = [ WORK,WORK,WORK, WORK, MOVE, MOVE, MOVE, CARRY, CARRY, CARRY, CARRY ];  } // 750
@@ -58,8 +58,37 @@ module.exports = function() {
       else if (roleName == "remote_harv") { var body = [ WORK, WORK, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, CARRY, CARRY, CARRY, CARRY ]; } // 700 }
       else if (roleName == "claimer")     { var body = [ WORK, WORK, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, CARRY, CARRY, CARRY, CARRY ];  }//  700 }
       else if (roleName == "claimer_lg")  { var body = [ CLAIM, MOVE, MOVE]; roleName = "claimer";  }// 700 }
-      else if (roleName == "soldier")     { var body = [ TOUGH,TOUGH,TOUGH,TOUGH,TOUGH,MOVE,MOVE,MOVE,MOVE,MOVE,MOVE,MOVE,MOVE,MOVE,ATTACK,ATTACK,ATTACK,ATTACK]; } // 900 }
       else if (roleName == "energy_mover"){ var body = [ WORK, WORK, WORK, WORK, WORK, MOVE, CARRY, CARRY ]; } // 1100}
+
+      // HEAL 250,RANGED_ATTACK 150,MOVE 50,ATTACK 80,TOUGH 10
+      // W = creep weight ( # of body parts, excluding move and carry)
+      // K = terrain factor ( .5 road, 1x plain, 5x swamp)
+      // M = number of move parts
+      // round up to the nearest integer
+      // t = ceil(K * W / M)
+
+      else if (roleName == "soldier") {
+        var body = [ TOUGH,TOUGH,TOUGH,TOUGH,                               // 40 - 4
+                     TOUGH,TOUGH,TOUGH,TOUGH,                               // 40 - 4
+                     MOVE,MOVE,MOVE,MOVE,MOVE,MOVE,MOVE,MOVE,MOVE,MOVE,     // 500 - 10
+                     MOVE,MOVE,                                             // 100 - 2
+                     ATTACK,ATTACK,ATTACK,ATTACK                            // 320 - 4
+                   ]; }     // 1000 total - balanced fatigue
+
+      else if (roleName == "soldier_rng") {
+        var body = [ TOUGH,TOUGH,TOUGH,TOUGH,TOUGH                          // 50 - 5
+                     TOUGH,TOUGH,TOUGH,TOUGH,TOUGH                          // 50 - 5
+                     MOVE,MOVE,MOVE,MOVE,MOVE,MOVE,MOVE,MOVE,MOVE,MOVE,     // 500 - 10
+                     MOVE,MOVE,                                             // 100 - 2
+                     RANGED_ATTACK,RANGED_ATTACK                            // 300 - 2
+                   ]; }     // 1000 total - balanced fatigue
+
+      else if (roleName == "soldier_heal"){
+        var body = [ TOUGH,TOUGH,TOUGH,TOUGH,                               // 40 - 4
+                     TOUGH,TOUGH,TOUGH,TOUGH,                               // 40 - 4
+                     MOVE,MOVE,MOVE,MOVE,MOVE,MOVE,MOVE,MOVE,MOVE,MOVE,     // 500 - 10
+                     HEAL,HEAL                                              // 500 - 2
+                   ]; }     //  1080 total - balanced fatigue
     }
 
     // create creep with the created body and the given role
--- role.soldier.js
+++ role.soldier.js
@@ -1,85 +1,53 @@
 module.exports = {
-    // a function to run the logic for this role
-    run: function(creep) {
-        creep.say("^-!");
+  // a function to run the logic for this role
+  run: function(creep) {
+    creep.say("^-!");
 
-        // get your assignment
-        var dest_key = creep.memory.destid;
-        if ( dest_key == 'Attack') {  var gotoFlag = Game.flags.Attack; }
-	else { var gotoFlag = creep.room.spawn; }
+    // get your assignment
+    var dest_key = creep.memory.destid;
+    if ( dest_key == 'Attack') {  var gotoFlag = Game.flags.Attack; }
+	  else { var gotoFlag = creep.room.spawn; }
 
-	// gotoFlag_id = "57e4ba80adafdf710cc4b5a9"; // his spawn
-	// var gotoFlag = Game.getObjectById("57e4ba80adafdf710cc4b5a9");
+	  // collect targets
+    var creep_target = creep.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
 
-        // collect targets
-        var creep_target = creep.pos.findClosestByRange(FIND_HOSTILE_CREEPS, {
-			filter: (s) => s.owner.username != 'nabo_c'
-	});
+  	var hostile_tower = creep.pos.findClosestByRange(FIND_HOSTILE_STRUCTURES, {
+  			filter: (s) => s.structureType==STRUCTURE_TOWER
+  	});
 
-	var hostile_tower = creep.pos.findClosestByRange(FIND_HOSTILE_STRUCTURES, {
-			filter: (s) => s.structureType==STRUCTURE_TOWER 
-	});
+  	var other_target = creep.pos.findClosestByRange(FIND_HOSTILE_STRUCTURES, {
+  			filter: (s) => s.structureType==STRUCTURE_SPAWN  ||
+  					s.structureType==STRUCTURE_EXTENSION
+  	});
 
-	var other_target = creep.pos.findClosestByRange(FIND_HOSTILE_STRUCTURES, {
-			filter: (s) => s.structureType==STRUCTURE_SPAWN  ||
-					s.structureType==STRUCTURE_EXTENSION
-	});
-
-        /*var neutral_controller = creep.pos.findClosestByRange(FIND_STRUCTURES, {
-                        filter: (s) => s.structureType==STRUCTURE_CONTROLLER &&
-					( s.owner.username != 'Arjuna'  ||
-					s.owner.username != 'nabo_c'
-					)
-
-        });*/
-
-	var claim_parts = creep.body.find( x => x.type == 'claim');
-	var heal_parts = creep.body.find( x => x.type == 'heal');
-
-        if( hostile_tower != undefined ) {
-                console.log(creep + "SOLDIER -- attacking hostile tower!" + hostile_tower);
-                if(creep.attack(hostile_tower) == ERR_NOT_IN_RANGE) {
-                        creep.moveTo(hostile_tower);
-                }
-        }
-	else if ( creep_target != undefined ) {
-		console.log(creep + "SOLDIER -- attacking hostile creep!" + creep_target);
-                if(creep.attack(creep_target) == ERR_NOT_IN_RANGE) {
-                        creep.moveTo(creep_target);
-                }
-	}
-        else if ( other_target != undefined ) {
-                console.log(creep + "SOLDIER -- attacking hostile target!" + other_target);
-                if(creep.attack(other_target) == ERR_NOT_IN_RANGE) {
-                        creep.moveTo(other_target);
-                }
-        }
-        /*
-	else if ( neutral_controller != undefined && claim_parts != undefined ) {
-                console.log(creep + "SOLDIER -- claiming target!" + neutral_controller);
-                if(creep.attack(neutral_controller) == ERR_NOT_IN_RANGE) {
-                        creep.moveTo(neutral_controller);
-                }
-        }
-	*/
-	else { 
-		creep.moveTo(gotoFlag); 
-	}
-
-/*
-        // are we in the room with the flag?
-        if ( creep.pos.roomName != gotoFlag.pos.roomName ) {
-                // not in the room with the flag, move towards the flag
-                moveStatus = creep.moveTo(gotoFlag);
-                console.log(creep + " soldier -movestatus " + moveStatus + " GF " + gotoFlag);
-                console.log(creep + " soldier -CR " + creep.pos.roomName + " FR: " + gotoFlag.pos.roomName);
-        }
-	else {
-		creep.moveTo(gotoFlag);
-	}
-*/
-
-// end
+    // attack tower
+    if( hostile_tower != undefined ) {
+            console.log(creep + "SOLDIER -- attacking hostile tower!" + hostile_tower);
+            if(creep.attack(hostile_tower) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(hostile_tower);
+            }
     }
-};
 
+    // attack hostile creep
+  	else if ( creep_target != undefined ) {
+  		console.log(creep + "SOLDIER -- attacking hostile creep!" + creep_target);
+        if(creep.attack(creep_target) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(creep_target);
+        }
+  	}
+
+    // attack spawn or extensions
+    else if ( other_target != undefined ) {
+      console.log(creep + "SOLDIER -- attacking hostile target!" + other_target);
+      if(creep.attack(other_target) == ERR_NOT_IN_RANGE) {
+              creep.moveTo(other_target);
+      }
+    }
+
+    // move towards the flag
+    else {
+  		creep.moveTo(gotoFlag);
+  	}
+
+  }
+};
