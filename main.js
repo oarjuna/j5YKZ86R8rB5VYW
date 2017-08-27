2fe4ae0b8746d6cd72d85642ee8f7e57d1a3e4a8
unknown <ryan.shannon@gmail.com>
2017-08-26 20:21:04 -0600

    asdfsd

--- main.js
+++ main.js
@@ -30,33 +30,35 @@ var roleSpawn = require('role.spawn');
   var scl[2] = [ 2, 1, 0, 0, 0, 0, 0, 0, 0];
 */
 
-var Empire = {
-  spawn_names: ['Spawn1','Spawn2','Spawn3','Spawn4'],
-  spawn_levels:
-              [
-                [ 4, 4, 5, 1, 0, 0, 1, 0, 0], // Spawn1
-                [ 3, 7, 1, 1, 0, 0, 1, 0, 0], // Spawn2
-                [ 1, 1, 0, 0, 0, 0, 0, 0, 0], // Spawn3
-                [ 1, 0, 0, 0, 0, 0, 0, 0, 0]  // Spawn4
-              ],
-  sources:    [
-                [ '5873bcc211e3e4361b4d81ec','5873bcc211e3e4361b4d81ed'], // Spawn1
-                [ '5873bcc511e3e4361b4d822e','5873bcc511e3e4361b4d822f'], // Spawn2
-                [ '5873bcc711e3e4361b4d827e','5873bcc711e3e4361b4d827f'], // Spawn3
-                [ '000000000000000000000001','000000000000000000000002']  // Spawn4
-              ],
-  harvs_per_source: [
-                      [2,2], // Spawn1
-                      [1,2], // Spawn2
-                      [1,1], // Spawn3
-                      [1,1]  // Spawn4
-                    ]
-};
+
 
 module.exports.loop = function () {
   var scl1 = [ 4, 4, 5, 1, 0, 0, 1, 0, 0];
   var scl2 = [ 3, 7, 1, 1, 0, 0, 1, 0, 0];
   var scl3 = [ 2, 1, 0, 0, 0, 0, 0, 0, 0];
+
+  var Empire = {
+    spawn_names: ['Spawn1','Spawn2','Spawn3','Spawn4'],
+    spawn_levels:
+                [
+                  [ 4, 4, 5, 1, 0, 0, 1, 0, 0], // Spawn1
+                  [ 3, 7, 1, 1, 0, 0, 1, 0, 0], // Spawn2
+                  [ 1, 1, 0, 0, 0, 0, 0, 0, 0], // Spawn3
+                  [ 1, 0, 0, 0, 0, 0, 0, 0, 0]  // Spawn4
+                ],
+    sources:    [
+                  [ '5873bcc211e3e4361b4d81ec','5873bcc211e3e4361b4d81ed'], // Spawn1
+                  [ '5873bcc511e3e4361b4d822e','5873bcc511e3e4361b4d822f'], // Spawn2
+                  [ '5873bcc711e3e4361b4d827e','5873bcc711e3e4361b4d827f'], // Spawn3
+                  [ '000000000000000000000001','000000000000000000000002']  // Spawn4
+                ],
+    harvs_per_source: [
+                        [2,2], // Spawn1
+                        [1,2], // Spawn2
+                        [1,1], // Spawn3
+                        [1,1]  // Spawn4
+                      ]
+  };
 
   for (let xx in Empire.spawn_names ) {
     [MinHarv,MinDeli,MinUgra,MinBuil,MinEner,MinReHa,MinClai,MinSold,MinRepa] =  Empire.spawn_levels[xx];
