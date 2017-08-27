1b417587f1c327ddf200b586169f31a6b9d6b952
unknown <ryan.shannon@gmail.com>
2017-08-27 11:30:43 -0600

    dd

--- status.js
+++ status.js
@@ -39,7 +39,8 @@ module.exports = {
           var source_energy = source.energy;
           var source_energy_cap = source.energyCapacity;
           var source_ticks = source.ticksToRegeneration;
-          console.log(spawn_name + " : " + source.id + " Ticks: " + source_ticks + " E: " + source_energy + "/" + source_energy_cap);
+
+          console.log(spawn_name + " : " + source_id + " Ticks: " + source_ticks + " E: " + source_energy + "/" + source_energy_cap);
         }
 
         // print the thing
