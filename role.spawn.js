7325371a88b22d3eca9bcddaa58c66984a074965
unknown <ryan.shannon@gmail.com>
2017-08-27 18:38:54 -0600

    ssss

--- role.spawn.js
+++ role.spawn.js
@@ -147,6 +147,7 @@ module.exports = {
 
         if (target != undefined) {
           console.log(tower + " -- attacking " + target);
+          Game.notify(tower + " -- attacking " + target,5);
           tower.attack(target);
         }
         else if (repair_target != undefined ) {
