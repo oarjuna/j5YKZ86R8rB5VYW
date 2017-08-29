c1a63bcb0078f1f94b504e399e7ffb6a9517b514
Ryan Shannon <shannon@visionlink.org>
2017-08-29 16:47:27 -0600

    foo

--- func.shared.js
+++ func.shared.js
@@ -6,9 +6,9 @@ module.exports = {
 		var container = creep.pos.findClosestByRange(FIND_STRUCTURES, {
     	filter: (s) =>
 				(
-					( s.structureType==STRUCTURE_CONTAINER && s.store[RESOURCE_ENERGY] > 90 ) ||
 					( s.structureType==STRUCTURE_LINK && s.energy[RESOURCE_ENERGY] > 90 ) ||
-					( s.structureType==STRUCTURE_STORAGE && s.store[RESOURCE_ENERGY] > 90 )
+					( s.structureType==STRUCTURE_STORAGE && s.store[RESOURCE_ENERGY] > 90 ) ||
+					( s.structureType==STRUCTURE_CONTAINER && s.store[RESOURCE_ENERGY] > 90 ) 
     )});
 
 		creep.say("++");
