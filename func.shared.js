ae92b01c915c48e2ae0b856b66a93f0dd3f5a031
unknown <ryan.shannon@gmail.com>
2017-08-30 17:05:13 -0600

    ddd

--- func.shared.js
+++ func.shared.js
@@ -4,22 +4,28 @@ module.exports = {
 	pickupEnergy: function(creep)
 	{
 
-		var container = creep.pos.findClosestByRange(FIND_STRUCTURES, {
-    	filter: (s) =>
-				(
-					( s.structureType==STRUCTURE_CONTAINER && s.store[RESOURCE_ENERGY] > 300 )
-    )});
+
+	// TODO ??????
+		if ( creep.memory.role == 'upgrader' && creep.memory.destid != '000000000000000000000001' ) {
+			// Find receiving link w/ energy
+			let rec_link_id = creep.memory.destid;
+			tmp_c = Game.getObjectById(rec_link_id);
+			console.log("xxxx " + tmp_c + " " + rec_link_id);
+			if ( tmp_c.energy > 90 ) { container = tmp_c; }
+		}
+
+		else {
+			// Find containers
+			var container = creep.pos.findClosestByRange(FIND_STRUCTURES, {
+	    	filter: (s) =>
+					(
+						( s.structureType==STRUCTURE_CONTAINER && s.store[RESOURCE_ENERGY] > 300 )
+	    )});
+		}
+
 
 		if ( container == undefined ) {
 			container = creep.room.storage;
-		}
-
-// TODO
-		if ( creep.memory.role == 'upgrader' && creep.memory.destid != '000000000000000000000001' ) {
-			let foo = creep.memory.destid;
-			tmp_c = Game.getObjectById(foo);
-			console.log("xxxx " + tmp_c + " " + foo);
-			if ( tmp_c.energy > 90 ) { container = tmp_c; }
 		}
 
 		creep.say("++");
