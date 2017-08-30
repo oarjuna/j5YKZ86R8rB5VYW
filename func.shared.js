466d294dda34c2f033af9b8b68412dc93915cee2
unknown <ryan.shannon@gmail.com>
2017-08-29 21:02:00 -0600

    dd

--- func.shared.js
+++ func.shared.js
@@ -16,7 +16,7 @@ module.exports = {
 
 		if ( creep.memory.role == 'upgrader' ) {
 			tmp_c = Game.getObjectById(creep.memory.destid);
-			if ( tmp_c.energy > 90 ) { container = tmp_c; }
+			//if ( tmp_c.energy > 90 ) { container = tmp_c; }
 		}
 
 		creep.say("++");
