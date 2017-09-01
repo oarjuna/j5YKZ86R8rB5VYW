3cdf598747996d45ce9fa0a6da9c2d39f8b631c4
unknown <ryan.shannon@gmail.com>
2017-09-01 05:52:36 -0600

    djhdh

--- main.js
+++ main.js
@@ -25,8 +25,8 @@ module.exports.loop = function () {
                 //  Harvest, Deliv, Upgrade, Build, Energy, RemoteHarv, Claim, Soldier, Repair, Sol-M ,Sol-R, Sol-H
                 [
                   [ 2, 3, 5, 1, 0, 0, 0, 0, 0, 0, 0, 0], // Spawn1
-                  [ 2, 3, 4, 1, 0, 0, 0, 0, 1, 0, 0, 0], // Spawn2
-                  [ 4, 3, 5, 1, 0, 0, 0, 0, 1, 0, 0, 0], // Spawn3
+                  [ 2, 3, 2, 1, 0, 0, 0, 0, 1, 0, 0, 0], // Spawn2
+                  [ 4, 3, 2, 1, 0, 0, 0, 0, 1, 0, 0, 0], // Spawn3
                   [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]  // Spawn4
                 ],
     sources:    [
