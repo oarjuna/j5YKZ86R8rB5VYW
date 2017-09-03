7e4d161602274921b0e0c3808b63c4c66740612e
unknown <ryan.shannon@gmail.com>
2017-09-02 22:49:19 -0600

    ss

--- main.js
+++ main.js
@@ -61,29 +61,6 @@ module.exports.loop = function () {
     military_squad : []
   };
 
-  function Squad(id,leader,members,movedest,attacktarget) {
-    this.id = id;
-    this.leader = leader;
-    this.member = members;
-    this.movedest = movedest;
-    this.attacktarget = attacktarget;
-    this.addMember = function (newMember) {
-      this.member.push(newMember);
-    }
-    this.delMember = function (rmMember) {
-      this.member.splice(rmMember,1);
-    }
-  }
-
-  var sq1 = new Squad(1,'leader1',['mem1','mem2','mem3'],'movedest','attack');
-  Hive.military_squad.push(sq1);
-  sq1.addMember('mem4');
-
-  console.log("A:" + Hive.military_squad[0].member[3]);
-  sq1.delMember(3);
-  console.log("A:" + Hive.military_squad[0].member[3]);
-
-  general.run(Hive);
 
   // housekeeping -- check for memory entries of dead creeps by iterating over Memory.creeps
   for (let name in Memory.creeps) { if (Game.creeps[name] == undefined) { delete Memory.creeps[name]; } }
@@ -105,6 +82,8 @@ module.exports.loop = function () {
   	else if (creep.memory.role == 'remote_harv')    { roleRemoteHarvester.run(creep); }
 
   }
+
+  general.run(Hive);
 
   roleSpawn.run(0,Hive);
   roleSpawn.run(1,Hive);
