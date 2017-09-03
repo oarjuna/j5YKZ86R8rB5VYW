c076f8411fec1ac9d944c15e0484e9cb520ecb09
unknown <ryan.shannon@gmail.com>
2017-09-03 17:04:35 -0600

    ss

--- role.spawn.js
+++ role.spawn.js
@@ -229,7 +229,7 @@ module.exports = {
     // detect idle creep that are empty
     var empty_creeps = _.filter(Game.creeps, (c) =>
       ( c.memory.birthplace == spawn_name ) &&
-      ( .sum(c.carry) == 0 ) &&
+      ( c.carryCapacity == _.sum(c.carry) ) &&
       ( c.memory.state == 'idle' )
     );
 
