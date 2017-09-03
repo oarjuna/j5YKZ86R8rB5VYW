7ccbb736bcb97d9d8318f4593427579ab55542d5
unknown <ryan.shannon@gmail.com>
2017-09-02 22:48:39 -0600

    ss

--- role.general.js
+++ role.general.js
@@ -17,9 +17,10 @@ module.exports = {
 
     var sq1 = new Squad(1,'leader1',['mem1','mem2','mem3'],'movedest','attack');
     Hive.military_squad.push(sq1);
-    sq1.addMember('mem4');
 
+    sq1.addMember('mem4');
     console.log("A:" + Hive.military_squad[0].member[3]);
+
     sq1.delMember(3);
     console.log("A:" + Hive.military_squad[0].member[3]);
 
