b27dc811491cf6bf80f9b4680ae06cb9de7290b0
unknown <ryan.shannon@gmail.com>
2017-09-03 20:37:48 -0600

    ss

--- role.spawn.js
+++ role.spawn.js
@@ -286,11 +286,11 @@ module.exports = {
     if ( containers.length != 0 ) {
       for ( let y of containers) {
         var job = new Job('01bb',1,'unassigned','deliverer',y.id,Game.time);
+        console.log("NS: job " + job);
         Hive.job_queue.push(job);
         y.memory.working_count -= deliver_carry_cap;
       }
     }
-    console.log("NS: job " + job);
 
 
     // assign idle creeps to unassinged jobs
