14f73d576c5486955e82e4356bbb95cf2ef96282
unknown <ryan.shannon@gmail.com>
2017-09-04 12:31:03 -0600

    ss

--- role.soldier.js
+++ role.soldier.js
@@ -5,7 +5,9 @@ module.exports = {
 
     // get your assignment
     var dest_key = creep.memory.destid;
-    if ( dest_key == 'Attack') {  var gotoFlag = Game.flags.Attack; }
+    if ( dest_key == 'Attack1') {  var gotoFlag = Game.flags.Attack1; }
+    if ( dest_key == 'Attack2') {  var gotoFlag = Game.flags.Attack2; }
+
 	  else { var gotoFlag = creep.room.spawn; }
 
 	  // collect targets
