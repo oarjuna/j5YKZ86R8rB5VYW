5a15694d7b714edacd75e531ebdfc28502941b69
unknown <ryan.shannon@gmail.com>
2017-09-04 14:16:23 -0600

    ss

--- role.soldier.js
+++ role.soldier.js
@@ -45,7 +45,9 @@ module.exports = {
     // attack hostile creep
   	 if ( creep_target != undefined ) {
   		console.log(creep + "SOLDIER -- attacking hostile creep!" + creep_target);
-        creep.attack(creep_target);
+        if(creep.attack(creep_target) == ERR_NOT_IN_RANGE) {
+          creep.moveTo(creep_target);
+      }
   	}
 
     // attack spawn or extensions
