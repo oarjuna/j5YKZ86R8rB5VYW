390ac7745e13a53bb55661389f9dc9c2a9849ad0
unknown <ryan.shannon@gmail.com>
2017-09-04 18:26:40 -0600

    ss

--- role.new-harvester.js
+++ role.new-harvester.js
@@ -47,20 +47,20 @@ module.exports = {
       if ( creep.room.controller.ticksToDowngrade < 200 ) {
           // Emergency controller upgrade
           var structure = creep.room.controller;
-          creep.say('\uD83D\uDE9A'); // 🚚
+          creep.say('\uD83D\uDE9A.ER'); // 🚚
       }
 
       else if ( structure_link[0] != undefined ){
         // Load the closest link
         var structure = structure_link[0];
-        creep.say('\uD83D\uDE9A'); // 🚚
+        creep.say('\uD83D\uDE9A.li'); // 🚚
         var deliv_link = true;
       }
 
       else if ( structure_container != null ) {
         // Load a container
         var structure = structure_container;
-        creep.say('\uD83D\uDE9A'); // 🚚
+        creep.say('\uD83D\uDE9A.st'); // 🚚
       }
 
       else {
