9b0a1e34d8d6261d40b3249226ccee91667d387f
unknown <ryan.shannon@gmail.com>
2017-09-04 18:09:03 -0600

    ss

--- role.deliverer.js
+++ role.deliverer.js
@@ -49,28 +49,28 @@ module.exports = {
       if ( creep.room.controller.ticksToDowngrade < 500 ) {
           // Emergency controller upgrade
           var structure = creep.room.controller;
-          creep.say("🚚.co");
+          creep.say("🚚 co");
       }
       else if ( structure_tower != null ) {
         // Load a container
         var structure = structure_tower;
-        creep.say("🚚.tw");
+        creep.say("🚚 tw");
       }
 
       else if (structure_spawn != null) {
         // Load the extension or spawn
         var structure = structure_spawn;
-        creep.say("🚚.se");
+        creep.say("🚚 se");
       }
       else if ( structure_link != undefined && structure_link.energy < structure_link.energyCapacity - 200) {
         // Load a sending link
         var structure = structure_link;
-        creep.say("🚚.li");
+        creep.say("🚚 li");
       }
       else {
         // drop off at storage
         var structure = structure_storage;
-        creep.say("🚚.st");
+        creep.say("🚚 st");
 	}
 
       if (creep.transfer(structure, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
