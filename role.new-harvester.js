2b0e2ea82e8763066afe710c65f968e879ebba8b
unknown <ryan.shannon@gmail.com>
2017-09-04 18:18:12 -0600

    ss

--- role.new-harvester.js
+++ role.new-harvester.js
@@ -47,26 +47,26 @@ module.exports = {
       if ( creep.room.controller.ticksToDowngrade < 200 ) {
           // Emergency controller upgrade
           var structure = creep.room.controller;
-          creep.say("🚚.ER");
+          creep.say("\u1F69A .ER");
       }
 
       else if ( structure_link[0] != undefined ){
         // Load the closest link
         var structure = structure_link[0];
-        creep.say("🚚.li");
+        creep.say("\u1F69A.li");
         var deliv_link = true;
       }
 
       else if ( structure_container != null ) {
         // Load a container
         var structure = structure_container;
-        creep.say("🚚.cn");
+        creep.say("\u1F69A.cn");
       }
 
       else {
         // drop off at storage
         var structure = structure_storage;
-        creep.say("🚚.ST");
+        creep.say("\u1F69A.ST");
 			}
 
     if (creep.transfer(structure, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
@@ -86,7 +86,7 @@ module.exports = {
       else {
         var source = target_source;
       }
-      creep.say('\u26CF');
+      creep.say('\u26CF'); //  ⛏
     //console.log(creep + " dest " + target_source);
 		  if (creep.harvest(source) == ERR_NOT_IN_RANGE) {
 		    creep.moveTo(source);
