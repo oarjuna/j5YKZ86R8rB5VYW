d21000239416816251c5dee2b750456489907951
unknown <ryan.shannon@gmail.com>
2017-09-09 10:03:56 -0600

    ss

--- role.spawn.js
+++ role.spawn.js
@@ -363,7 +363,7 @@ module.exports = {
           // record the desired resource state
           var adjustmest = y.memory.working_count - deliver_carry_cap;
           if ( adjustmest < 0 ) { adjustmest = 0; }
-          console.log("XX: " + spawn_name + " id: " + y.id + " curr: " + y.memory.working_count + " --adj: " + adjustmest );
+          console.log(\t"XX: " + spawn_name + " id: " + y.id + " curr: " + y.memory.working_count + " --adj: " + adjustmest );
           y.memory.working_count = y.memory.working_count - deliver_carry_cap;
           if ( y.memory.working_count < 0 ) { y.memory.working_count = 0;  }
         }
