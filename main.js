67900fb650ad96d13cb49cebec0019084f5f811d
unknown <ryan.shannon@gmail.com>
2017-09-08 21:01:49 -0600

    ss

--- main.js
+++ main.js
@@ -111,6 +111,7 @@ Object.defineProperty(StructureController.prototype, 'alert_state', {
   configurable: true
 });
 */
+
 // xsinx
 Creep.prototype.hasActiveBodypart = function (type) {
 	for (let i = this.body.length - 1; i >= 0; i--) {
