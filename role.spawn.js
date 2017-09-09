8a87dc4f2137a3ca60f9812bc955da73c2b404ae
unknown <ryan.shannon@gmail.com>
2017-09-08 20:12:04 -0600

    ss

--- role.spawn.js
+++ role.spawn.js
@@ -329,7 +329,7 @@ module.exports = {
           ( _.sum(c.carry) == empty ) &&
           ( c.memory.state == 'idle' ) &&
           ( c.memory.role == 'harvester' ) &&
-          ( c.memory.destid == dest_id )
+          ( c.memory.destid == job.dest_id )
         );
 
         console.log(spawn_name + " ha: " + harvester);
