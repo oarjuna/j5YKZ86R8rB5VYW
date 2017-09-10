8947a53d09ec60a162dc6d37fcf11df6f41e2b32
unknown <ryan.shannon@gmail.com>
2017-09-09 19:45:15 -0600

    ss

--- role.spawn.js
+++ role.spawn.js
@@ -422,7 +422,7 @@ module.exports = {
         if ( structure_link[0] != undefined ) {
           console.log("\t link : full harv dest: " + structure_link[0]);
         }
-        else if ( container != 'undefined') {
+        else if ( container != undefined ) {
           console.log("\t cont: full harv dest: " + container);
         }
 
