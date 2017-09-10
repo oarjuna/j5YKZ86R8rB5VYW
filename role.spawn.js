19158d8ee6569b7055ca1221e7c964f149bac5d8
unknown <ryan.shannon@gmail.com>
2017-09-09 19:42:19 -0600

    ss

--- role.spawn.js
+++ role.spawn.js
@@ -420,10 +420,10 @@ module.exports = {
         )});
 
         if ( structure_link[0] != 'undefined') {
-          console.log("\t JQ: full harv dest: " + structure_link[0]);
+          console.log("\t link : full harv dest: " + structure_link[0]);
         }
         else if ( container != 'undefined') {
-          console.log("\t JQ: full harv dest: " + container);
+          console.log("\t cont: full harv dest: " + container);
         }
 
 
