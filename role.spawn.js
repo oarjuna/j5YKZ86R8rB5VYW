b0afab6af59aca26066c4a560f60b40f60c404f1
unknown <ryan.shannon@gmail.com>
2017-09-09 18:30:12 -0600

    gg

--- role.spawn.js
+++ role.spawn.js
@@ -346,9 +346,9 @@ module.exports = {
 
     // 01 - fillfrom - aa - source - harvesters
     for ( let source of sources ) { // foreach source in the room
-      // search jobs for any jobs with a dest_id == source AND job.state == unassigned
+      // search jobs for any jobs with a dest_id == source
       let result = _.find(Hive.memory.job_queue, function(o) {
-        return ( o.dest_id == source && o.state == 'unassigned' );
+        return ( o.dest_id == source );
       });
 
       if ( result == undefined ) {
