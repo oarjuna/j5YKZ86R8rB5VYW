dc05c90d8b4364fe1d29d5bd5b91828b9de3b5a3
unknown <ryan.shannon@gmail.com>
2017-09-09 20:17:01 -0600

    ss

--- role.spawn.js
+++ role.spawn.js
@@ -435,6 +435,8 @@ module.exports = {
         if ( dest != undefined ) {
           var job = new Job(spawn_name,'02aa',1,'assigned','harvester',dest.id,Game.time,'');
           console.log("JQ: " + spawn_name + " newjob " + dest.id + " job " + job.type + " j_id: " + job.id );
+          // push the job onto the job_queue
+          Hive.memory.job_queue.push(job);s
           // assign the job to the creep
           harv.memory.job = job.id;
           // mark the job as assigned
