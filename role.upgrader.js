65adf613113ddddaf68e415c3ed6b7888f3c4722
unknown <ryan.shannon@gmail.com>
2017-09-13 16:03:33 -0600

    ss

--- role.upgrader.js
+++ role.upgrader.js
@@ -42,11 +42,7 @@ module.exports = {
               }
             }
 
-/*
-            if (creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(creep.room.controller);
-            }
-*/
+
 
         }
         else {
