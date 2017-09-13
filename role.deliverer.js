c4b1908c814fd7e351f3efa84a03f868681588ab
unknown <ryan.shannon@gmail.com>
2017-09-13 17:30:26 -0600

    ss

--- role.deliverer.js
+++ role.deliverer.js
@@ -27,71 +27,8 @@ module.exports = {
     if (creep.memory.working == true) {
       // When Full
       // Decide where to go
-
-      // Find the SPAWN and extensions
-      var structure_spawn = creep.pos.findClosestByRange(FIND_MY_STRUCTURES, {
-        filter: (s) => (
-          ( s.structureType == STRUCTURE_SPAWN && s.energy < s.energyCapacity ) ||
-          ( s.structureType == STRUCTURE_EXTENSION && s.energy < s.energyCapacity )
-        )});
-
-      // Find Towers
-      var structure_tower = creep.pos.findClosestByPath(FIND_MY_STRUCTURES, {
-         filter: (s) => (
-            ( s.structureType == STRUCTURE_TOWER && s.energy < s.energyCapacity - 250 )
-          )});
-
-	    // Find storage
-	    var structure_storage = creep.room.storage;
-
-      // find the sending links
-      var rec_link = creep.memory.destid;
-      var structure_link = creep.pos.findClosestByPath(FIND_MY_STRUCTURES, {
-        filter: (s) => (
-          ( s.structureType == STRUCTURE_LINK && s.id != rec_link )
-      )});
-
-
-
-      if ( creep.room.controller.ticksToDowngrade < 500 ) {
-          // Emergency controller upgrade
-          var structure = creep.room.controller;
-          creep.say('\uD83D\uDE9A.ER'); // 🚚
-      }
-
-      else if ( creep.room.controller.memory.alert_state == 'red' && structure_tower != null ) {
-        // Load a container
-        var structure = structure_tower;
-        creep.say('\u26A0.tw'); // 🚚
-      }
-
-      else if ( structure_spawn != null ) {
-        // Load the extension or spawn
-        var structure = structure_spawn;
-        creep.say('\uD83D\uDE9A.+sp'); // 🚚;
-      }
-
-      else if ( structure_tower != null ) {
-        // Load a container
-        var structure = structure_tower;
-        creep.say('\uD83D\uDE9A.+tw'); // 🚚
-      }
-
-      else if ( structure_link != undefined && structure_link.energy < structure_link.energyCapacity - 500) {
-        // Load a sending link
-        var structure = structure_link;
-        creep.say('\uD83D\uDE9A.+li'); // 🚚
-      }
-
-      else {
-        // drop off at storage
+      if ( creep.memory.carrytype == 'mineral' ) {
         var structure = structure_storage;
-        creep.say('\uD83D\uDE9A.+st'); // 🚚
-	     }
-
-       if ( creep.memory.carrytype == 'mineral' ) {
-         var structure = structure_storage;
-       }
 
        for(const resourceType in creep.carry) {
            status = creep.transfer(structure, resourceType);
@@ -99,6 +36,71 @@ module.exports = {
                creep.moveTo(structure);
            }
        }
+      }
+      else {
+        // Find the SPAWN and extensions
+        var structure_spawn = creep.pos.findClosestByRange(FIND_MY_STRUCTURES, {
+          filter: (s) => (
+            ( s.structureType == STRUCTURE_SPAWN && s.energy < s.energyCapacity ) ||
+            ( s.structureType == STRUCTURE_EXTENSION && s.energy < s.energyCapacity )
+          )});
+
+        // Find Towers
+        var structure_tower = creep.pos.findClosestByPath(FIND_MY_STRUCTURES, {
+           filter: (s) => (
+              ( s.structureType == STRUCTURE_TOWER && s.energy < s.energyCapacity - 250 )
+            )});
+
+  	    // Find storage
+  	    var structure_storage = creep.room.storage;
+
+        // find the sending links
+        var rec_link = creep.memory.destid;
+        var structure_link = creep.pos.findClosestByPath(FIND_MY_STRUCTURES, {
+          filter: (s) => (
+            ( s.structureType == STRUCTURE_LINK && s.id != rec_link )
+        )});
+
+
+
+        if ( creep.room.controller.ticksToDowngrade < 500 ) {
+            // Emergency controller upgrade
+            var structure = creep.room.controller;
+            creep.say('\uD83D\uDE9A.ER'); // 🚚
+        }
+
+        else if ( creep.room.controller.memory.alert_state == 'red' && structure_tower != null ) {
+          // Load a container
+          var structure = structure_tower;
+          creep.say('\u26A0.tw'); // 🚚
+        }
+
+        else if ( structure_spawn != null ) {
+          // Load the extension or spawn
+          var structure = structure_spawn;
+          creep.say('\uD83D\uDE9A.+sp'); // 🚚;
+        }
+
+        else if ( structure_tower != null ) {
+          // Load a container
+          var structure = structure_tower;
+          creep.say('\uD83D\uDE9A.+tw'); // 🚚
+        }
+
+        else if ( structure_link != undefined && structure_link.energy < structure_link.energyCapacity - 500) {
+          // Load a sending link
+          var structure = structure_link;
+          creep.say('\uD83D\uDE9A.+li'); // 🚚
+        }
+
+        else {
+          // drop off at storage
+          var structure = structure_storage;
+          creep.say('\uD83D\uDE9A.+st'); // 🚚
+  	     }
+       }
+
+
 
       } // End UNLOAD
 
