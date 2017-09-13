0894cf1cdc63e8394d7a0fcc1b0654cf7900d00c
unknown <ryan.shannon@gmail.com>
2017-09-13 15:55:42 -0600

    ss

--- role.upgrader.js
+++ role.upgrader.js
@@ -31,7 +31,7 @@ module.exports = {
               roleDeliverer.run(creep);
             }
             else {
-              if (creep.pos.findInRange(creep.room.controller,2 )) {
+              if (creep.pos.findInRange(creep.room.controller,1 )) {
                 creep.upgradeController(creep.room.controller)
               }
               else {
