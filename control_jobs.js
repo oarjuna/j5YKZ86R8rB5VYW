549d49c2f851ce21722591b9c48529d73fc021e1
unknown <ryan.shannon@gmail.com>
2017-09-15 08:47:16 -0600

    ss

--- control_jobs.js
+++ control_jobs.js
@@ -8,32 +8,7 @@ module.exports = {
     // job states - assigned / complete / abandoned / timed out
     // creep state - source_bound / dest_bound / working / idle
 
-    // job object prototype - spawn_name,type, priority, state, body_type_req, dest_id, extra, tick_issued, tick_complete
-    //    states - assigned / complete / abandoned / timed out / unasssigned
-    function Job (spawn_name,type,priority,state,body_type_req,dest_id,extra,tick_issued,tick_complete) {
-        this.spawn_name = spawn_name;
-        this.type = type;
-        this.priority = priority;
-        this.state = state;
-        this.body_type_req = body_type_req;
-        this.dest_id = dest_id;
-        this.extra = extra;
-        this.tick_issued = tick_issued;
-        this.tick_complete = tick_complete;
-        this.uuid = function uuid()  {
-          var uuid = "", i, random,uuidlen = 12;
-
-          for (i = 0; i < uuidlen; i++) {
-            random = Math.random() * 16 | 0;
-            if (i == 8 || i == 12 || i == 16 || i == 20) {
-              uuid += "-";
-            }
-            uuid += (i == 12 ? 4 : (i == 16 ? (random & 3 | 8) : random)).toString(16);
-          }
-          return uuid;
-        };
-        this.id = this.uuid();
-    }
+    
 
     // variables
     var container_energy_floor = 100;
