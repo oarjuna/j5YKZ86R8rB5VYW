4b8b1d09fe7bd1bba01201af081d3616ad7fe5da
unknown <ryan.shannon@gmail.com>
2017-09-14 20:40:26 -0600

    ss

--- role.generic-creep.js
+++ role.generic-creep.js
@@ -8,7 +8,8 @@ module.exports = {
 			creep.memory.state = 'working';
 			// receive orders
 			// find your job...
-			var job = _.find(Hive.memory.job_queue, {'uuid' : assigned_job}); // TODO.	
+			var job = _.find(Hive.memory.job_queue,  function(o) { return o.uuid == assigned_job; }); // TODO.
+
 			var type = job.type;
 			var type2 = type.slice(0,2);
 			var type3 = type.slice(2);
