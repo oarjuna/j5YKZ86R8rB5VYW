8756ae0f3a29701a20f772f8bde40831254a1f73
unknown <ryan.shannon@gmail.com>
2017-09-15 08:47:04 -0600

    ss

--- control_planning.js
+++ control_planning.js
@@ -3,6 +3,33 @@ module.exports = {
     [MinHarv,MinDeli,MinUgra,MinBuil,MinClReg,MinReHa,MinClai,MinSold,MinRepa,MinSolM,MinSolR,MinSolH] =  Hive.spawn_levels[spawn_num];
     var spawn_name = Hive.spawn_names[spawn_num];
 
+    // job object prototype - spawn_name,type, priority, state, body_type_req, dest_id, extra, tick_issued, tick_complete
+    //    states - assigned / complete / abandoned / timed out / unasssigned
+    function Job (spawn_name,type,priority,state,body_type_req,dest_id,extra,tick_issued,tick_complete) {
+        this.spawn_name = spawn_name;
+        this.type = type;
+        this.priority = priority;
+        this.state = state;
+        this.body_type_req = body_type_req;
+        this.dest_id = dest_id;
+        this.extra = extra;
+        this.tick_issued = tick_issued;
+        this.tick_complete = tick_complete;
+        this.uuid = function uuid()  {
+          var uuid = "", i, random,uuidlen = 12;
+
+          for (i = 0; i < uuidlen; i++) {
+            random = Math.random() * 16 | 0;
+            if (i == 8 || i == 12 || i == 16 || i == 20) {
+              uuid += "-";
+            }
+            uuid += (i == 12 ? 4 : (i == 16 ? (random & 3 | 8) : random)).toString(16);
+          }
+          return uuid;
+        };
+        this.id = this.uuid();
+    }
+    
     // Debugging - clear the queue or add jobs to it
 
     if (Memory.clearqueue == true ){
