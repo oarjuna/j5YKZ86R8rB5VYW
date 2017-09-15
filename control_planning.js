e85fc8cb6d655345268fb67de6f6b08fb172f95d
unknown <ryan.shannon@gmail.com>
2017-09-15 16:51:56 -0600

    ss

--- control_planning.js
+++ control_planning.js
@@ -114,7 +114,7 @@ module.exports = {
     if ( res_containers.length > 0) {
       // foreach container needing pickup, see if there is a job already
       for ( let x of res_containers ) {
-        Log.debug("PL: " + x + " "+ x.store);
+        Log.debug("PL: " + x + " "+ _.keys(x.store);
         var foo = _.filter(Hive.memory.job_queue, function(s) {
           return  (
             ( s.type == '01bb' || s.type == '01jj' ) &&
