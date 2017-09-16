c6aaa5a0b33a94d88382ac1572ab350cda2962bc
unknown <ryan.shannon@gmail.com>
2017-09-15 20:42:33 -0600

    ss

--- main.js
+++ main.js
@@ -264,7 +264,7 @@ Creep.prototype.hasActiveBodypart = function (type) {
   for ( let job of Hive.memory.job_queue) {
 
     let tmp_obj = Game.getObjectById(job.assigned_creep);
-    if ( tmp_obj.name != undefined ) { name = tmp_obj.name;}
+    if ( tmp_obj != undefined ) { name = tmp_obj.name;}
     else { name = 'undefined'; }
 
     console.log("JQ: " + job.spawn_name+ " j_id: " + job.id + " type " + job.type + " st: " + job.state + " cr: " + name + " d: " + job.dest_id + " extra: " + job.extra + " time_in_q " + ( Game.time - job.tick_issued));
