8e33eba48827b806436a15c533dd1a3f45245905
unknown <ryan.shannon@gmail.com>
2017-09-15 19:41:01 -0600

    ss

--- control_planning.js
+++ control_planning.js
@@ -137,7 +137,7 @@ module.exports = {
 
             if ( job_count.length < 999999  ) {
               var job = new Job(spawn_name,'01bb',1,'unassigned','deliverer',x.id,res,Game.time,'');
-              Hive.memory.job_queue.push(job);
+              //Hive.memory.job_queue.push(job);
               Log.debug("JQ: ADDING : " + spawn_name + " newjob " + job.id + " type " + job.type + " res " + res + " dest " + x.id);
             }
 
