37d26c0d8026b5ac138f336812912551ee479f1c
unknown <ryan.shannon@gmail.com>
2017-09-15 21:35:24 -0600

    ss

--- control_planning.js
+++ control_planning.js
@@ -154,7 +154,7 @@ module.exports = {
             if ( need_count[res] == undefined ) { need_count[res] = 1;}
             else {need_count[res]++; }
 
-            //Log.debug("PL: " + x + " RES " + res + " " + x.store[res] + " ex jobs: " + job_count.length + " need: " + need_count[res]);
+            Log.debug("PL: " + x + " RES " + res + " " + x.store[res] + " ex jobs: " + job_count.length + " need: " + need_count[res]);
 
             if ( job_count.length < need_count[res]  ) {
               var job = new Job(spawn_name,'01bb',1,'unassigned','deliverer',x.id,res,Game.time,'','');
