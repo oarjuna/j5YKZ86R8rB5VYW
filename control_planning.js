cb8a0f362e4c19b6d5ec2dcd21e0dee1514950a5
unknown <ryan.shannon@gmail.com>
2017-09-15 20:08:19 -0600

    ss

--- control_planning.js
+++ control_planning.js
@@ -130,9 +130,9 @@ module.exports = {
 
             // count how many jobs we need per resource type
             if (! need_count.indexOf(res) ) { need_count.push(res);}
-            need_count[res][0]++;
+            //need_count[res][0]++;
 
-            Log.debug("PL: " + x + " RES " + res + " " + x.store[res] + " ex jobs: " + job_count.length + " need: " + need_count[res][0]);
+            Log.debug("PL: " + x + " RES " + res + " " + x.store[res] + " ex jobs: " + job_count.length + " need: " + need_count[res]);
 
             if ( job_count.length < 999999  ) {
               var job = new Job(spawn_name,'01bb',1,'unassigned','deliverer',x.id,res,Game.time,'');
