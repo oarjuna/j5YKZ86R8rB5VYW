3dc1287e070087d7788c1387d27528b51a78e8b5
unknown <ryan.shannon@gmail.com>
2017-09-15 22:09:07 -0600

    ss

--- main.js
+++ main.js
@@ -267,7 +267,7 @@ console.log("------ START ------");
     if ( tmp_obj != undefined ) { name = tmp_obj.name;}
     else { name = 'undefined'; }
 
-    console.log("JQ: " + job.spawn_name+ " j_id: " + job.id + " type " + job.type + " st: " + job.state + " cr: " + name + " d: " + job.dest_id + " ex: " + job.extra + " t_in_q " + ( Game.time - job.tick_issued));
+    console.log("JQ: " + job.spawn_name+ " j_id: " + job.id + " type " + job.type + " st: " + job.state + " cr: " + name + " d: " + job.dest_id + " ex: " + job.extra + " tq " + ( Game.time - job.tick_issued));
   }
   console.log("------ END ------");
   //console.log("#-------------------------------------------------------#  H, U, R, B, RH, C, De, EM, S");
