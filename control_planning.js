fd27b86b8d901f4ae968520a0b4a7a899d4d5f8f
unknown <ryan.shannon@gmail.com>
2017-09-15 20:09:25 -0600

    ss

--- control_planning.js
+++ control_planning.js
@@ -130,7 +130,7 @@ module.exports = {
 
             // count how many jobs we need per resource type
             if (! need_count.indexOf(res) ) { need_count.push(res); need_count[res] = 0;}
-            need_count[res]++;
+            //need_count[res]++;
 
             Log.debug("PL: " + x + " RES " + res + " " + x.store[res] + " ex jobs: " + job_count.length + " need: " + need_count[res]);
 
