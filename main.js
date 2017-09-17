e16a20b7ffc30f0d3483ffea85a7399d9d18ef47
unknown <ryan.shannon@gmail.com>
2017-09-16 19:54:52 -0600

    ss

--- main.js
+++ main.js
@@ -275,7 +275,7 @@ console.log("------ START ------");
     if ( job.dest_id != 'closest' ) { var dest_obj = Game.getObjectById(job.dest_id); dest_type = dest_obj.structureType; }
     else { dest_type = job.dest_id; }
 
-    console.log("JQ: " + job.spawn_name+ " j_id: " + job.id + " type " + job.type + " st: " + job.state + " cr: " + name + " d: " + job.dest_id + " ex: " + job.extra + " tq " + ( Game.time - job.tick_issued));
+    console.log("JQ: " + job.spawn_name+ " j_id: " + job.id + " type " + job.type + " st: " + job.state + " cr: " + name + " d: " + dest_type + " ex: " + job.extra + " tq " + ( Game.time - job.tick_issued));
   }
   console.log("------ END ------");
   //console.log("#-------------------------------------------------------#  H, U, R, B, RH, C, De, EM, S");
