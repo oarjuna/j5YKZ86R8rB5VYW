ade41b6491593e1c33e9527f4d9724b0b8d1bb23
unknown <ryan.shannon@gmail.com>
2017-09-17 18:51:08 -0600

    ss

--- main.js
+++ main.js
@@ -292,7 +292,7 @@ console.log("------ START ------");
 
 
     if ( job.state == 'assigned') {
-      Log.info(job.spawn_name+ " j_id: " + job.id + "\tt: " + j_type + "\tp: " + job.priority + "\td: " + dest_type + "\ttq " + ( Game.time - job.tick_issued) + "\tcr: " + name ,'Queue');
+      Log.info(job.id + "@" + job.spawn_name + "\tt: " + j_type + "\tp: " + job.priority + "\td: " + dest_type + "\ttq " + ( Game.time - job.tick_issued) + "\tcr: " + name ,'Queue');
     }
   }
   console.log("------ END ------");
