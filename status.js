a36f577c54f9853c0c8409aa25edc11cd9a131be
Ryan Shannon <shannon@visionlink.org>
2017-09-19 08:23:44 -0600

    foo

--- status.js
+++ status.js
@@ -59,7 +59,7 @@ module.exports = {
         if (spawn_name == 'Spawn3') { var url = "<A HREF=https://screeps.com/a/#!/room/shard0/W26S81  style=color:" + Game.spawns[spawn_name].room.controller.memory.alert_state + ";\">Spawn3</a>"; }
         if (spawn_name == 'Spawn4') { var url = "<A HREF=https://screeps.com/a/#!/room/shard0/W25S82  style=color:" + Game.spawns[spawn_name].room.controller.memory.alert_state + ";\">Spawn4</a>"; }
 
-        Log.info(url + " : lvl" + lvl + status11 + status12 + " C: "+ total_status + " --\t" + status1 + status2 + status8 + status3 + status4 + status5 + status7 + status9 + status10);
+        Log.info(url + " : lvl" + lvl + status11 + status12 + " C: "+ total_status + " -- " + status1 + status2 + status8 + status3 + status4 + status5 + status7 + status9 + status10);
         //console.log(url + " : lvl" + lvl + status11 + status12 + " C: "+ total_status + " -- " + status1 + status2 + status3 + status4 + status5 + status7 + status8 + status9 + status10);
 
 
