bb2cc3af2a4e2522c45320758507fa1b2dd01cae
unknown <ryan.shannon@gmail.com>
2017-09-19 20:26:57 -0600

    ss

--- main.js
+++ main.js
@@ -148,7 +148,7 @@ Creep.prototype.hasActiveBodypart = function (type) {
                         [2,1]  // Spawn4
                       ],
     links: [
-            ['59a411fd06b5240409f8b847','59a446a802722c0fd674afdf'], // Spawn1
+            ['59c1cd62baf545049a7066d6','59a446a802722c0fd674afdf'], // Spawn1
             ['59c1b83f1561805c9cb25161','59c1bae1b3aa556157a63c16'], // Spawn2
             ['59ad5f7548ab72269b30f527','59ad81d6cdfbbf72a0df0c3a'], // Spawn3
             ['59c1c1911a916c343fd50d4a','59c1bc24b983a432a49d6461'], // Spawn4
