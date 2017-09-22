d53b56963c1108fb84f0afadb07665a5fc693c1f
unknown <ryan.shannon@gmail.com>
2017-09-22 15:27:39 -0600

    ss

--- control_job_planning.js
+++ control_job_planning.js
@@ -178,8 +178,8 @@ module.exports = {
         else { res_list = x.store }
   */
         var res_list = [RESOURCE_ENERGY,RESOURCE_OXYGEN];
-        for ( var res in x.store ) { // for each resource type we're dealing with  // TODO -- here!!
-        //for ( var res in res_list ) { // for each resource type we're dealing with
+        for ( var res in x.store ) { // for each resource type we're dealing with  // TODO -- swap this
+        //for ( var res in res_list ) { // for each resource type we're dealing with // TODO == with this
           if ( ( x.structureType == STRUCTURE_LINK && x.energy > 0 ) ||  ( x.store[res] != undefined && x.store[res] > 0 )) { // that is not empty
 
             Log.warn("RES cont type " + x.structureType,'Planner');
