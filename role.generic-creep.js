5914daca08bf1e68900c35af8c3812e80d46106d
unknown <ryan.shannon@gmail.com>
2017-09-21 20:26:32 -0600

    sss

--- role.generic-creep.js
+++ role.generic-creep.js
@@ -69,7 +69,7 @@ module.exports = {
 
 						if ( result == undefined ) {
 							// no, there must be a container 1 sq away. move there.
-							var close_containers = creep.pos.findInRange(STRUCTURE_CONTAINER,1);
+							var close_containers = creep.pos.findInRange(STRUCTURE_CONTAINER,2);
 							creep.travelTo(close_containers[0]);
 						}
 						else {
