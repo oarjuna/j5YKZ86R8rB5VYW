9450867f87f409f4a5aa1d7f0e611808ec136703
unknown <ryan.shannon@gmail.com>
2017-09-22 14:32:04 -0600

    ss

--- control_job_planning.js
+++ control_job_planning.js
@@ -172,10 +172,12 @@ module.exports = {
 
     if ( res_pickup_spots.length > 0) { // if there are res_pickup_spots needing pickup
       for ( var x of res_pickup_spots ) { // for each res_pickup_spots with stuff
+        if ( x.structureType == STRUCTURE_LINK ) { var res_list = RESOURCE_ENERGY; }
+        else { res_list = x.store }
         for ( var res in x.store ) { // for each resource type in each container's store list
           if ( x.store[res] > 0 || ( x.structureType == STRUCTURE_LINK && x.energy > 0 ) ) { // that is not empty
 
-            Log.debug("RES cont type " + x.structureType,'Planner');
+            Log.debug("RES cont type " + x.store,'Planner');
 
             // get the amount of resource to pickup = resources / carry cap
             num_of_jobs_needed =  x.store[res] / Hive.deliverer_carry_cap[spawn_num];
