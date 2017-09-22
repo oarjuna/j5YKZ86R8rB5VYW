b01700dec3beac7ab9426e98c1651c66beab2abc
unknown <ryan.shannon@gmail.com>
2017-09-22 17:00:00 -0600

    ss

--- control_job_planning.js
+++ control_job_planning.js
@@ -178,9 +178,9 @@ module.exports = {
         else { res_list = x.store }
   */
         var res_list = [RESOURCE_ENERGY,RESOURCE_OXYGEN];
-        //for ( var res in x.store ) { // for each resource type we're dealing with  // TODO -- swap this
-        for ( var res in res_list ) { // for each resource type we're dealing with // TODO == with this
-        if ( x.store == undefined ) { continue; }
+        for ( var res in x.store ) { // for each resource type we're dealing with  // TODO -- swap this
+        //for ( var res in res_list ) { // for each resource type we're dealing with // TODO == with this
+        //if ( x.store == undefined ) { continue; }
 
           if (
             ( x.structureType == STRUCTURE_LINK && x.energy > 0 && res == RESOURCE_ENERGY ) ||
