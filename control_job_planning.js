aff02cb45dd5faa19442481655486c4ec1920c8b
unknown <ryan.shannon@gmail.com>
2017-09-22 12:49:44 -0600

    ss

--- control_job_planning.js
+++ control_job_planning.js
@@ -183,8 +183,9 @@ module.exports = {
             if ( job_count.length < num_of_jobs_needed ) {
               // spawn a new job
 
-              // if x.structureType == STRUCTURE_CONTAINER
-              var job = new Job(spawn_name,'01bb',4,'unassigned','deliv_or_upgrd',x.id,res,Game.time,'','');
+              if x.structureType == STRUCTURE_CONTAINER
+                var job = new Job(spawn_name,'01bb',4,'unassigned','deliv_or_upgrd',x.id,res,Game.time,'','');
+              }
               // else if x.structureType == STRUCTURE_LINK
              // var job = new Job(spawn_name,'01dd',4,'unassigned','upgrader',x.id,res,Game.time,'','');
 
