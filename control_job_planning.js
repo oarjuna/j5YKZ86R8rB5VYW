165f0324352d037159dc03aa9b21c25779af2cb1
unknown <ryan.shannon@gmail.com>
2017-09-22 14:47:00 -0600

    ssss

--- control_job_planning.js
+++ control_job_planning.js
@@ -62,7 +62,7 @@ module.exports = {
         Memory.clearjob = 'xxx';
         Log.debug("removed " + removed,'Planner');
     }
-return;
+
     // TYPES OF actions
     /////// FILL
     // Fillfrom - 01aa - resource -> harv
