072274dec465945a9d8a6071ebf2b50056e50d14
unknown <ryan.shannon@gmail.com>
2017-09-22 17:14:07 -0600

    sss

--- control_job_planning.js
+++ control_job_planning.js
@@ -195,7 +195,7 @@ module.exports = {
             ( x.structureType == STRUCTURE_CONTAINER && eng_check > 0 ))
           {
 
-            Log.warn("Res: " + res + " res_list " + x.store[RESOURCE_OXYGEN],'Planner');
+            Log.warn("Res: " + res + " res_list " + eng_check,'Planner');
             // get the amount of resource to pickup = resources / carry cap
             if ( x.structureType == STRUCTURE_CONTAINER) {
               num_of_jobs_needed =  x.store[res] / Hive.deliverer_carry_cap[spawn_num];
