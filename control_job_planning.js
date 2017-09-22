eed1b7491f42898c73702fe6ccafe7fc39689af6
unknown <ryan.shannon@gmail.com>
2017-09-22 13:54:39 -0600

    ss

--- control_job_planning.js
+++ control_job_planning.js
@@ -167,12 +167,11 @@ module.exports = {
           ( s.structureType== STRUCTURE_LINK && s.id == Hive.receiving_link[spawn_num] && s.energy >= Hive.upgrader_carry_cap[spawn_num] )
     )});
 
-    var res_pickup_spots_tmp = _.union(res_pickup_spots_cont,res_pickup_spots_links)
 
+    var res_pickup_spots = res_pickup_spots_cont;
+    var res_pickup_spots_tmp = _.union(res_pickup_spots_cont,res_pickup_spots_links);
     Log.debug("RES: " + res_pickup_spots_tmp,'Planner' );
 
-    //var res_pickup_spots = _.merge(res_pickup_spots_cont,res_pickup_spots_links)
-    var res_pickup_spots = res_pickup_spots_cont;
 
     if ( res_pickup_spots.length > 0) { // if there are res_pickup_spots needing pickup
       for ( var x of res_pickup_spots ) { // for each res_pickup_spots with stuff
