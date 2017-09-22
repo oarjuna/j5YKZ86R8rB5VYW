54a359a68e091d778a5937fd51d9e90187430af0
unknown <ryan.shannon@gmail.com>
2017-09-22 12:23:14 -0600

    ss

--- control_job_planning.js
+++ control_job_planning.js
@@ -98,7 +98,7 @@ module.exports = {
 
   //
   var num_of_upgraders = Hive.spawn_levels[spawn_num][2]; // deliverers
-  Log.debug(" num of up " + num_of_upgraders + " num of jobs " + upgrader_job_list.length,"Planner");
+  Log.debug(" num of up " + num_of_upgraders + " num of jobs " + upgrader_job_list,"Planner");
 
 
    /*idle_upgraders = _.filter(Game.creeps, (c) =>
