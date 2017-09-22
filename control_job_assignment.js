301c648e5e28caa3a73d11b6d74e0500ed354f5c
unknown <ryan.shannon@gmail.com>
2017-09-22 15:35:04 -0600

    ss

--- control_job_assignment.js
+++ control_job_assignment.js
@@ -16,7 +16,7 @@ module.exports = {
     var harvester_carry_cap = 150;
     var job_TTL = 200;
     var tmpcreep, min_needed;
-return;
+//return;
     // when a job is completed, times out, or is abandoned, remove it from the job_queue
     // find those jobs and remove them
     var removed = _.remove(Hive.memory.job_queue, function(s) {
