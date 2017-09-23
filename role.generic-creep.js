bfb954126239c2e6bcfad360d624a3301a05003f
unknown <ryan.shannon@gmail.com>
2017-09-22 18:58:59 -0600

    ss

--- role.generic-creep.js
+++ role.generic-creep.js
@@ -122,8 +122,10 @@ module.exports = {
 
 				//if ( dest_obj.energy == dest_obj.energyCapacity ||  _.sum(creep.carry) == 0 ) { var complete = true; }
 
-				if ( dest_obj.structureType == STRUCTURE_CONTROLLER && _.sum(creep.carry) == 0 ) { var complete = true; }
-				else if ( dest_obj.energy == dest_obj.energyCapacity ||  _.sum(creep.carry) == 0 ){ var complete = true; }
+				if ( dest_obj.structureType == STRUCTURE_CONTROLLER && _.sum(creep.carry) == 0 ) {
+					var complete = true;
+				}
+				else if (  dest_obj.structureType ! = STRUCTURE_CONTROLLER && ( dest_obj.energy == dest_obj.energyCapacity ||  _.sum(creep.carry) == 0 ) ){ var complete = true; }
 
 			} // END DELIV
 
