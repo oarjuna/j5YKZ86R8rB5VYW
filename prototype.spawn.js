e1a6bbf7d23a7bed10060124d2dd93131222414e
unknown <ryan.shannon@gmail.com>
2017-09-22 20:04:28 -0600

    ss

--- prototype.spawn.js
+++ prototype.spawn.js
@@ -15,7 +15,7 @@ module.exports = function() {
     else if ( birthplace == 'Spawn2' && roleName == 'harvester') { var ryantest = true; }
 
     else if ( birthplace == 'Spawn1' && roleName == 'deliverer') { var ryantest = true; }
-    //else if ( birthplace == 'Spawn2' && roleName == 'deliverer') { var ryantest = true; }
+    else if ( birthplace == 'Spawn2' && roleName == 'deliverer') { var ryantest = true; }
 
     else if ( birthplace == 'Spawn1' && roleName == 'upgrader') { var ryantest = true; }
   //  else if ( birthplace == 'Spawn2' && roleName == 'upgrader') { var ryantest = true; }
