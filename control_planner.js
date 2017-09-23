23b45ccffbea53e103b47f6e5a83f45f63ef8355
unknown <ryan.shannon@gmail.com>
2017-09-22 22:57:08 -0600

    ss

--- control_planner.js
+++ control_planner.js
@@ -2,7 +2,7 @@ var plan_harvs = require('control_planner-harvs');
 
 module.exports = {
   run: function(spawn_num,Hive) {
-    [MinHarv,MinDeli,MinUgra,MinBuil,MinClReg,MinReHa,MinClai,MinSold,MinRepa,MinSolM,MinSolR,MinSolH] =  Hive.spawn_levels[spawn_num];
+  //  [MinHarv,MinDeli,MinUgra,MinBuil,MinClReg,MinReHa,MinClai,MinSold,MinRepa,MinSolM,MinSolR,MinSolH] =  Hive.spawn_levels[spawn_num];
     var spawn_name = Hive.spawn_names[spawn_num];
 
     //if ( spawn_name != 'Spawn1' && spawn_name != 'Spawn2') { return; }
