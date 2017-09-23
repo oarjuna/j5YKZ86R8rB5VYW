b45a408f3a73133c24aa9028e375ff3be8edc14a
unknown <ryan.shannon@gmail.com>
2017-09-22 20:05:13 -0600

    ss

--- prototype.spawn.js
+++ prototype.spawn.js
@@ -18,7 +18,7 @@ module.exports = function() {
     else if ( birthplace == 'Spawn2' && roleName == 'deliverer') { var ryantest = true; }
 
     else if ( birthplace == 'Spawn1' && roleName == 'upgrader') { var ryantest = true; }
-  //  else if ( birthplace == 'Spawn2' && roleName == 'upgrader') { var ryantest = true; }
+    else if ( birthplace == 'Spawn2' && roleName == 'upgrader') { var ryantest = true; }
 
     else { var ryantest = false; }
     //var ryantest = false;
