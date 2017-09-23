d2358ca7d77e85239382402dd4384011d294b268
unknown <ryan.shannon@gmail.com>
2017-09-22 22:53:39 -0600

    ss

--- control_spawn.js
+++ control_spawn.js
@@ -1,7 +1,7 @@
 var jobcontrol = require('control_job_assignment');
 var towercontrol = require('control_towers');
 var linkcontrol = require('control_links');
-var plancontrol = require('control_job_planning');
+var plancontrol = require('control_planner');
 
 module.exports = {
     /*
