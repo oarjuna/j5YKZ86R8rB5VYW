08d53f245b49482f14bee5adfeb9f5f51b34dde1
unknown <ryan.shannon@gmail.com>
2017-09-22 23:35:30 -0600

    ss

--- control_planner.js
+++ control_planner.js
@@ -7,7 +7,34 @@ module.exports = {
     //if ( spawn_name != 'Spawn1' && spawn_name != 'Spawn2') { return; }
     //if ( spawn_name != 'Spawn1') { return; }
 
+    // Define the job object
+    // job object prototype - spawn_name,type, priority, state, body_type_req, dest_id, extra, tick_issued, tick_complete
+    //    states - assigned / complete / abandoned / timed out / unasssigned
+    function Job (spawn_name,type,priority,state,body_type_req,dest_id,extra,tick_issued,tick_complete,assigned_creep) {
+        this.spawn_name = spawn_name;
+        this.type = type;
+        this.priority = priority;
+        this.state = state;
+        this.body_type_req = body_type_req;
+        this.dest_id = dest_id;
+        this.extra = extra;
+        this.tick_issued = tick_issued;
+        this.tick_complete = tick_complete;
+        this.assigned_creep = assigned_creep;
+        this.uuid = function uuid()  {
+          var uuid = "", i, random,uuidlen = 12;
 
+          for (i = 0; i < uuidlen; i++) {
+            random = Math.random() * 16 | 0;
+            if (i == 8 || i == 12 || i == 16 || i == 20) {
+              uuid += "-";
+            }
+            uuid += (i == 12 ? 4 : (i == 16 ? (random & 3 | 8) : random)).toString(16);
+          }
+          return uuid;
+        };
+        this.id = this.uuid();
+    }
 
     // Manual commands for testing
     if (Memory.clearqueue == true ){
--- main.js
+++ main.js
@@ -24,34 +24,6 @@ var general = require('role.general');
 var Traveler = require('Traveler');
 global.Log = require('Log')
 
-// Define the job object
-// job object prototype - spawn_name,type, priority, state, body_type_req, dest_id, extra, tick_issued, tick_complete
-//    states - assigned / complete / abandoned / timed out / unasssigned
-function Job (spawn_name,type,priority,state,body_type_req,dest_id,extra,tick_issued,tick_complete,assigned_creep) {
-    this.spawn_name = spawn_name;
-    this.type = type;
-    this.priority = priority;
-    this.state = state;
-    this.body_type_req = body_type_req;
-    this.dest_id = dest_id;
-    this.extra = extra;
-    this.tick_issued = tick_issued;
-    this.tick_complete = tick_complete;
-    this.assigned_creep = assigned_creep;
-    this.uuid = function uuid()  {
-      var uuid = "", i, random,uuidlen = 12;
-
-      for (i = 0; i < uuidlen; i++) {
-        random = Math.random() * 16 | 0;
-        if (i == 8 || i == 12 || i == 16 || i == 20) {
-          uuid += "-";
-        }
-        uuid += (i == 12 ? 4 : (i == 16 ? (random & 3 | 8) : random)).toString(16);
-      }
-      return uuid;
-    };
-    this.id = this.uuid();
-}
 
 // START
 module.exports.loop = function () {
