1d2f46b611777d2f9a831af62ba5da7b6a77b7b0
unknown <ryan.shannon@gmail.com>
2017-09-24 10:03:33 -0600

    ss

--- control_planner-market.js
+++ control_planner-market.js
@@ -85,8 +85,8 @@ module.exports = {
 
     if ( Memory.sell != false ) {
       var order_id = Memory.sell;
-      //var status = Game.market.deal(order_id, amountToSell,  Game.spawns[spawn_name].room.name);
-    //  Log.debug("Selling to: " + order_id + " Status: " + status );
+      var status = Game.market.deal(order_id, amountToSell,  Game.spawns[spawn_name].room.name);
+      Log.debug("Selling to: " + order_id + " Status: " + status );
       Memory.sell = false;
     }
 
