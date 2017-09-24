66b28b34fc8a961ab6826c6b9b16d1f79183bccb
unknown <ryan.shannon@gmail.com>
2017-09-24 10:05:07 -0600

    ss

--- control_planner-market.js
+++ control_planner-market.js
@@ -85,6 +85,7 @@ module.exports = {
 
     if ( Memory.sell != false ) {
       var order_id = Memory.sell;
+        const amountToSell = 1000;
       var status = Game.market.deal(order_id, amountToSell,  Game.spawns[spawn_name].room.name);
       Log.debug("Selling to: " + order_id + " Status: " + status );
       Memory.sell = false;
