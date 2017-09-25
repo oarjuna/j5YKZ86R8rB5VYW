4107b9d85149f9e0e0900db4f499e46a28e7369e
unknown <ryan.shannon@gmail.com>
2017-09-24 22:06:32 -0600

    ss

--- role.claimer.js
+++ role.claimer.js
@@ -36,7 +36,7 @@ module.exports = {
 						( s.structureType == STRUCTURE_CONTAINER || s.structureType == STRUCTURE_ROAD )
 			});
 
-			//repairSite = undefined;
+			repairSite = undefined;
 
 			// Switch states
 			if (creep.memory.working == true && creep.carry.energy == 0) {
