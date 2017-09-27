3b815626763dc4c241325c04d15137ee9c773df6
unknown <ryan.shannon@gmail.com>
2017-09-27 17:09:19 -0600

    ss

--- role.generic-creep.js
+++ role.generic-creep.js
@@ -46,6 +46,7 @@ module.exports = {
 				return;
 			}
 
+			Log.warn( creep + " dest obj type " + dest_obj.structureType);
 			if ( job.type == '02ii' && dest_obj.structureType == STRUCTURE_CONTROLLER ) {
 				Log.warn(creep + " no storage, changing to 02ff !! ",'Generic');
 				job.type = '02ff';
