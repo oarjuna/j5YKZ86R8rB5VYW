40ce7ebda22c0ee1b139c2450ddb8b80928298e9
unknown <ryan.shannon@gmail.com>
2017-09-27 20:05:52 -0600

    sss

--- main.js
+++ main.js
@@ -133,7 +133,7 @@ Creep.prototype.hasActiveBodypart = function (type) {
                 [// H  D  U  B Cr  Rh C  S  R Sm Sr SH
                   [ 3, 2, 2, 1, 0, 0, 0, 1, 0, 0, 0, 0], // Spawn1
                   [ 3, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0], // Spawn2
-                  [ 1, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], // Spawn3
+                  [ 2, 3, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0], // Spawn3
                   [ 3, 2, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0],  // Spawn4
                   [ 2, 1, 4, 0, 0, 0, 0, 0, 1, 0, 0, 0]  // Spawn5
 
